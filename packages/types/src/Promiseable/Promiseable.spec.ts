e4eba3614b0da148cd5f6aa1130e1be5f5cedd58
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-05-02 01:37:40 +0900

    feat(types): ExtractSetType, ExtractMapType, Promiseable 추가 (#94)

---
+++ packages/types/src/Promiseable/Promiseable.spec.ts
@@ -0,0 +1,19 @@
+import { Promiseable } from '.';
+
+describe('Promiseable', () => {
+  it('should correctly handle and type-check a variable as either Promise<number> or number when assigned with Promiseable<number>', () => {
+    const promise = Promise.resolve(1) as Promiseable<number>;
+
+    expectTypeOf(promise).toEqualTypeOf<Promise<number> | number>();
+  });
+
+  it('should correctly distinguish between Promise<number> and number type at runtime when using Promiseable<number>', () => {
+    const promise = Promise.resolve(1) as Promiseable<number>;
+
+    if (promise instanceof Promise) {
+      expectTypeOf(promise).toEqualTypeOf<Promise<number>>();
+    } else {
+      expectTypeOf(promise).toEqualTypeOf<number>();
+    }
+  });
+});
