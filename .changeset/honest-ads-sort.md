8f039d380537e12cb1d2fd5dd8f875a930986b41
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-05-01 17:45:20 +0900

    feat(utils): isPromise 함수 추가 (#90)

---
+++ .changeset/honest-ads-sort.md
@@ -0,0 +1,5 @@
+---
+'@modern-kit/utils': patch
+---
+
+feat: isPromise 함수 및 테스트 코드 추가
---
+++ packages/utils/src/validator/isPromise/isPromise.spec.ts
@@ -0,0 +1,23 @@
+import { isPromise } from '.';
+
+describe('isPromise', () => {
+  it('should return true if the given value is a Promise', () => {
+    expect(isPromise(Promise.resolve())).toBeTruthy();
+
+    expect(isPromise(() => {})).toBeFalsy();
+    expect(isPromise('123')).toBeFalsy();
+    expect(isPromise(true)).toBeFalsy();
+    expect(isPromise({})).toBeFalsy();
+    expect(isPromise(null)).toBeFalsy();
+  });
+
+  it('should narrow the type to Promise if isPromise returns true', () => {
+    const promise: any = Promise.resolve(1);
+
+    if (isPromise<number>(promise)) {
+      expectTypeOf(promise).toEqualTypeOf<Promise<number>>();
+    } else {
+      expectTypeOf(promise).toEqualTypeOf<any>();
+    }
+  });
+});
