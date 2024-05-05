82a54ed69f35c2f7254eed12d751f92ca2b1e359
soonki-98 <alstnsrl98@gmail.com>
2024-05-05 16:31:01 +0900

    test(#100): update test case

--- packages/react/src/components/SwitchCase/SwitchCase.test.tsx
+++ packages/react/src/components/SwitchCase/SwitchCase.test.tsx
@@ -1,17 +1,56 @@
-/*
- * Created on Thu Nov 16 2023
- *
- * Copyright (c) 2023 Your Company
- */
+import { screen } from '@testing-library/react';
 
-import { render } from '@testing-library/react';
-import SwitchCase from '.';
+import { renderSetup } from '../../utils/test/renderSetup';
+
+import { SwitchCase } from '.';
 
 describe('SwitchCase', () => {
-  it('SwitchCase는 조건을 받을 수 있어야 한다.', () => {
-    const { container } = render(
-      <SwitchCase condition={0 as number} cases={{ 0: <div>1</div> }} />
+  it('should SwitchCase component receive condition.', () => {
+    renderSetup(
+      <SwitchCase
+        condition={0 as number}
+        cases={{ 0: <button>case no.1</button> }}
+      />
     );
-    expect(container.querySelector('div')).toBeInTheDocument();
+    const CaseOneComponent = screen.queryByRole('button', {
+      name: 'case no.1',
+    });
+
+    expect(CaseOneComponent).toBeInTheDocument();
+  });
+
+  it('should render defaultCaseComponent when there is no matched condition.', () => {
+    renderSetup(
+      <SwitchCase
+        condition={0 as number}
+        cases={{ 1: <button>case no.1</button> }}
+        defaultCaseComponent={<button>default component</button>}
+      />
+    );
+
+    const CaseOneComponent = screen.queryByRole('button', {
+      name: 'case no.1',
+    });
+    const DefaultCaseComponent = screen.queryByRole('button', {
+      name: 'default component',
+    });
+
+    expect(CaseOneComponent).not.toBeInTheDocument();
+    expect(DefaultCaseComponent).toBeInTheDocument();
+  });
+
+  it('should render defaultCaseComponent when condition is nullable.', () => {
+    renderSetup(
+      <SwitchCase
+        condition={null}
+        cases={{}}
+        defaultCaseComponent={<button>default component</button>}
+      />
+    );
+
+    const DefaultCaseComponent = screen.queryByRole('button', {
+      name: 'default component',
+    });
+    expect(DefaultCaseComponent).toBeInTheDocument();
   });
 });
