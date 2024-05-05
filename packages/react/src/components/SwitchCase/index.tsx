9fc8bb2f8b48d63ca451126e31d37da378a4eb9e
soonki-98 <alstnsrl98@gmail.com>
2024-05-05 16:30:23 +0900

    feat(#100): change props name defaultCase into defaultCaseComponent

--- packages/react/src/components/SwitchCase/index.tsx
+++ packages/react/src/components/SwitchCase/index.tsx
@@ -1,39 +1,21 @@
-type Component = JSX.Element | null;
+import { Nullable } from '@modern-kit/types';
 
-interface Props<Condition extends string | number> {
+type Component = Nullable<React.ReactNode>;
+
+interface SwitchCaseProps<Condition extends string | number> {
   condition: Condition | null | undefined;
   cases: Partial<Record<Condition, Component>>;
-  defaultCase?: Component;
+  defaultCaseComponent?: Component;
 }
 
-/**
- * @example
- * ```tsx
- * const [testValue, setTestValue] = useState(0);
- *
- * <SwitchCase
- * 	condition={testValue}
- * 	cases={{
- * 		0: <Comp1 />,
- * 		1: <Comp2 />,
- *  	2: <Comp3 />
- * 	}}
- * 	defaultCase={<Comp4 />}
- * />
- * ```
- *
- * @param props.condition - 비교 조건
- * @param props.cases - 조건에 부합하면 렌더링될 컴포넌트
- * @param props.defaultCase - 조건에 부합하는 값이 없으면 렌더링할 컴포넌트
- */
 export const SwitchCase = <Condition extends string | number>({
   condition,
   cases,
-  defaultCase = null,
-}: Props<Condition>) => {
-  if (condition === null || condition === undefined) {
-    return defaultCase;
+  defaultCaseComponent = null,
+}: SwitchCaseProps<Condition>) => {
+  if (condition == null) {
+    return defaultCaseComponent;
   }
 
-  return cases[condition] ?? defaultCase;
+  return cases[condition] ?? defaultCaseComponent;
 };
