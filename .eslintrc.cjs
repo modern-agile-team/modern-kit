d03d4542069a62dce63de60a6c9005134cfcab73
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-05-08 15:19:59 +0900

    docs: When 컴포넌트 문서 수정 (#109)

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,7 +7,7 @@ module.exports = {
     'plugin:@typescript-eslint/recommended',
     'plugin:react-hooks/recommended',
   ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  ignorePatterns: ['dist', '.eslintrc.*', 'babel.*'],
   parser: '@typescript-eslint/parser',
   plugins: ['unused-imports', 'react-refresh'],
   rules: {
