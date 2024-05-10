5defea9a18678bf9dbd2ae988d84e27e27f62fd6
ssi02014 <ssi02014@naver.com>
2024-05-10 19:08:42 +0900

    chore: @modern-kit/utils string isValidEmail export 제거

--- packages/utils/src/string/index.ts
+++ packages/utils/src/string/index.ts
@@ -1,5 +1,4 @@
 export * from './countSubstringOccurrences';
 export * from './extractNumber';
 export * from './removeSpecialCharacters';
-export * from '../validator/isValidEmail';
 export * from './repeatCharacters';
