179d6b2b4c0bc02f15fc9df194b356244677da5d
sangminnn <psm0102psm@gmail.com>
2024-05-12 00:29:41 +0900

    imp(utils): parseJson함수 내 조건식 위치 변경 및 환경변수 체크구문 제거 완료

--- packages/utils/src/common/parseJson/index.ts
+++ packages/utils/src/common/parseJson/index.ts
@@ -1,18 +1,16 @@
 export const parseJSON = <T>(value: any): T | null => {
-  if (value === '') {
-    return '' as T;
-  }
-
   if (typeof value !== 'string') {
     return value as T;
+  }
+
+  if (value === '') {
+    return '' as T;
   }
 
   try {
     return JSON.parse(value) as T;
   } catch {
-    if (process.env.NODE_ENV === 'development') {
-      console.error(`데이터를 파싱하는 데에 실패했습니다. 원본: ${value}`);
-    }
+    console.error(`데이터를 파싱하는 데에 실패했습니다. 원본: ${value}`);
     return null;
   }
 };
