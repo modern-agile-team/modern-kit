e648f1ddc07a0fe27301dc4191f247638537f1dd
hoonloper <yonghoonj98@gmail.com>
2024-05-24 22:11:40 +0900

    test(validators): isFunction 테스트 추가

--- packages/utils/src/validator/isFunction/isFunction.spec.ts
+++ packages/utils/src/validator/isFunction/isFunction.spec.ts
@@ -1,15 +1,54 @@
 import { isFunction } from '.';
 
 describe('isFunction', () => {
-  it('should return true if the argument is a function and false otherwise', () => {
-    function example() {}
+  it('should return true for arrow function', () => {
+    expect(isFunction(() => {})).toBe(true);
+  });
 
-    expect(isFunction(() => {})).toBeTruthy();
-    expect(isFunction(example)).toBeTruthy();
+  it('should return true for anonymous function', () => {
+    expect(isFunction(function () {})).toBe(true);
+  });
 
-    expect(isFunction('123')).toBeFalsy();
-    expect(isFunction(123)).toBeFalsy();
-    expect(isFunction({})).toBeFalsy();
-    expect(isFunction([])).toBeFalsy();
+  it('should return true for named function', () => {
+    expect(isFunction(function namedFunction() {})).toBe(true);
+  });
+
+  it('should return true for class method', () => {
+    class MyClass {
+      method() {}
+    }
+    expect(isFunction(new MyClass().method)).toBe(true);
+  });
+
+  it('should return true for Function constructor', () => {
+    expect(isFunction(new Function())).toBe(true);
+  });
+
+  it('should return false for object', () => {
+    expect(isFunction({})).toBe(false);
+  });
+
+  it('should return false for number', () => {
+    expect(isFunction(123)).toBe(false);
+  });
+
+  it('should return false for string', () => {
+    expect(isFunction('test')).toBe(false);
+  });
+
+  it('should return false for boolean', () => {
+    expect(isFunction(true)).toBe(false);
+  });
+
+  it('should return false for array', () => {
+    expect(isFunction([])).toBe(false);
+  });
+
+  it('should return false for null', () => {
+    expect(isFunction(null)).toBe(false);
+  });
+
+  it('should return false for undefined', () => {
+    expect(isFunction(undefined)).toBe(false);
   });
 });
