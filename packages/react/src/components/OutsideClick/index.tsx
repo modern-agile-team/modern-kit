bb9ae3fff16f3e667e4d8e824d70f8701a5bb38d
Collection50 <fkaus65@naver.com>
2024-05-24 16:21:02 +0900

    fix: OutsideClick 컴포넌트가 모든 react 태그를 수용하고 직관적인 ㅣ타입 가딩이 가능할 수 있도록 타입 수정 및 로직 수정

--- packages/react/src/components/OutsideClick/index.tsx
+++ packages/react/src/components/OutsideClick/index.tsx
@@ -1,16 +1,50 @@
-import { ComponentProps, ElementType, ReactNode } from 'react';
+import {
+  ComponentProps,
+  ElementType,
+  ReactNode,
+  PropsWithChildren,
+} from 'react';
 import { useOnClickOutside } from '../../hooks/useOnClickOutside';
 
-type OutsideClickProp<Tag extends ElementType> = ComponentProps<Tag> & {
-  children: ReactNode;
-} & {
-  as?: Tag;
-  callback: () => void;
-};
+type NonHaveChildElements =
+  | 'input'
+  | 'textarea'
+  | 'img'
+  | 'br'
+  | 'hr'
+  | 'meta'
+  | 'link'
+  | 'base'
+  | 'col'
+  | 'embed'
+  | 'source'
+  | 'track'
+  | 'wbr';
 
-export function OutsideClick<
-  E extends HTMLElement = HTMLDivElement,
-  Tag extends ElementType = 'div'
+type NoChildren<Tag extends ElementType> = Tag extends NonHaveChildElements
+  ? { children?: never }
+  : { children?: ReactNode };
+
+type AsRequired<Tag extends ElementType> = Tag extends 'div'
+  ? { as?: Tag }
+  : { as: Tag };
+
+type AllowedTagName<Tag extends ElementType> =
+  Tag extends keyof HTMLElementTagNameMap
+    ? HTMLElementTagNameMap[Tag]
+    : HTMLElement;
+
+type OutsideClickProp<Tag extends ElementType> = PropsWithChildren<
+  ComponentProps<Tag>
+> &
+  AsRequired<Tag> &
+  NoChildren<Tag> & {
+    callback: () => void;
+  };
+
+export default function OutsideClick<
+  Tag extends ElementType = 'div',
+  E extends AllowedTagName<Tag> = AllowedTagName<Tag>
 >({ as, children, callback, ...props }: OutsideClickProp<Tag>) {
   const { ref } = useOnClickOutside<E>(callback);
 
