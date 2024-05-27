04ab39e13be9e19f63d9fe4a277034fa40ba9004
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-05-28 01:15:48 +0900

    fix(react): useInterval enabled 속성 추가 (#173)
    * fix(react): useInterval enabled 속성 추가
    
    * fix(react): useInterval options 개선
    
    * docs: useInterval 문서 수정

--- packages/react/src/hooks/useInterval/index.ts
+++ packages/react/src/hooks/useInterval/index.ts
@@ -1,21 +1,33 @@
+import { isNumber } from '@modern-kit/utils';
 import { usePreservedCallback } from '../usePreservedCallback';
 import { useEffect } from 'react';
 
 type SetIntervalParameters = Parameters<typeof setInterval>;
 
+type IntervalOptions =
+  | number
+  | { delay: SetIntervalParameters[1]; enabled?: boolean };
+
 export const useInterval = (
   callback: SetIntervalParameters[0],
-  delay?: SetIntervalParameters[1]
+  options: IntervalOptions
 ) => {
+  const delay = isNumber(options) ? options : options.delay;
+  const enabled = isNumber(options) ? true : options?.enabled ?? true;
+
   const callbackAction = usePreservedCallback(callback);
 
   useEffect(() => {
-    if (delay == null) {
-      return;
-    }
+    if (delay == null) return;
 
-    const intervalId = window.setInterval(callbackAction, delay);
+    const intervalId = window.setInterval(() => {
+      if (!enabled) {
+        clearInterval(intervalId);
+      } else {
+        callbackAction();
+      }
+    }, delay);
 
     return () => clearInterval(intervalId);
-  }, [callbackAction, delay]);
+  }, [callbackAction, enabled, delay]);
 };
