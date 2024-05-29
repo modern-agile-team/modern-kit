1c389aa56e1511f6428a6059e88b09bc34696317
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-05-29 15:49:29 +0900

    feat(utils): rem cache 처리 (#177)
    * feat(utils): rem cache 처리
    
    Co-authored-by: minsoo-web <https://github.com/minsoo-web>
    Co-authored-by: Sangminnn <https://github.com/Sangminnn>
    
    * docs: rem 문서 수정
    
    * test(react): rem 테스트 코드 수정

--- packages/utils/src/style/rem/rem.spec.ts
+++ packages/utils/src/style/rem/rem.spec.ts
@@ -16,35 +16,36 @@ afterEach(() => {
 
 describe('rem', () => {
   it('should convert pixels to rem without any options. (Root FontSize: 16px)', () => {
-    expect(rem(16)).toBe(1);
-    expect(rem(24)).toBe(1.5);
-    expect(rem(32)).toBe(2);
+    expect(rem(16)).toBe('1rem');
+    expect(rem(20)).toBe('1.25rem');
+    expect(rem(32)).toBe('2rem');
+    expect(rem(32)).toBe('2rem'); // cached
   });
 
   it('should convert pixels to rem without any options. (Root FontSize: 24px)', () => {
     getComputedStyleSpy.mockReturnValue({ fontSize: '24px' });
 
-    expect(rem(24)).toBe(1);
-    expect(rem(30)).toBe(1.25);
-    expect(rem(36)).toBe(1.5);
+    expect(rem(24)).toBe('1rem');
+    expect(rem(30)).toBe('1.25rem');
+    expect(rem(36)).toBe('1.5rem');
   });
 
-  it('should convert pixels to rem and add "rem" suffix. (Root FontSize: 16px)', () => {
-    expect(rem(16, { suffix: true })).toBe('1rem');
-    expect(rem(24, { suffix: true })).toBe('1.5rem');
-    expect(rem(32, { suffix: true })).toBe('2rem');
+  it('should convert pixels to rem and remove "rem" suffix. (Root FontSize: 16px)', () => {
+    expect(rem(16, { suffix: false })).toBe(1);
+    expect(rem(24, { suffix: false })).toBe(1.5);
+    expect(rem(32, { suffix: false })).toBe(2);
   });
 
   it('should convert pixels to rem with fixed decimal places. (Root FontSize: 16px)', () => {
-    expect(rem(17)).toBe(1.0625);
+    expect(rem(17)).toBe('1.0625rem');
 
-    expect(rem(17, { toFixedDigits: 0 })).toBe(1); // toFixed Default behavior
-    expect(rem(17, { toFixedDigits: NaN })).toBe(1); // toFixed Default behavior
-    expect(rem(17, { toFixedDigits: 1 })).toBe(1.1);
-    expect(rem(17, { toFixedDigits: 2 })).toBe(1.06);
-    expect(rem(17, { toFixedDigits: 3 })).toBe(1.063);
-    expect(rem(17, { toFixedDigits: 4 })).toBe(1.0625);
-    expect(rem(17, { toFixedDigits: 10 })).toBe(1.0625);
+    expect(rem(17, { toFixedDigits: NaN })).toBe('1rem');
+    expect(rem(17, { toFixedDigits: 0 })).toBe('1rem');
+    expect(rem(17, { toFixedDigits: 1 })).toBe('1.1rem');
+    expect(rem(17, { toFixedDigits: 2 })).toBe('1.06rem');
+    expect(rem(17, { toFixedDigits: 3 })).toBe('1.063rem');
+    expect(rem(17, { toFixedDigits: 4 })).toBe('1.0625rem');
+    expect(rem(17, { toFixedDigits: 10 })).toBe('1.0625rem');
   });
 
   it('should throw an error if toFixedDigits is out of range.', () => {
