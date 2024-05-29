5c8d17b06b418bd0f4a3b4b3fae4b9367d8bdc0d
Collection50 <fkaus65@naver.com>
2024-05-30 01:02:57 +0900

    fix: 메서드 이름을 exclude -> excludeElements로 변경

---
+++ packages/utils/src/array/excludeElements/index.ts
@@ -0,0 +1,5 @@
+export function excludeElements<E>(array: E[], ...args: E[]) {
+  const excludeSet = new Set(args.map((arg) => JSON.stringify(arg)));
+
+  return array.filter((element) => !excludeSet.has(JSON.stringify(element)));
+}
