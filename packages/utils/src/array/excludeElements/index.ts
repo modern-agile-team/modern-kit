ca388a1ee5bdae779dba53d0a62c1eb372c32715
Collection50 <fkaus65@naver.com>
2024-05-30 15:12:56 +0900

    fix: excludeElements가 2개의 제네릭을 사용할 수 있도록 매개변수 타입 수정

--- packages/utils/src/array/excludeElements/index.ts
+++ packages/utils/src/array/excludeElements/index.ts
@@ -1,5 +1,8 @@
-export function excludeElements<E>(array: E[], ...args: E[]) {
+export const excludeElements = <E, U extends E>(
+  array: ReadonlyArray<E> | Array<E>,
+  ...args: ReadonlyArray<U> | Array<U>
+): Array<E> => {
   const excludeSet = new Set(args.map((arg) => JSON.stringify(arg)));
 
   return array.filter((element) => !excludeSet.has(JSON.stringify(element)));
-}
+};
