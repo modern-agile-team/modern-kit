bf4b6034b7997a56cead9b8b16d49a71bd78787a
Collection50 <fkaus65@naver.com>
2024-05-31 18:23:38 +0900

    fix: chunk의 2번째 인수의 제네릭 타입 추가

--- packages/utils/src/array/chunk/index.ts
+++ packages/utils/src/array/chunk/index.ts
@@ -1,6 +1,9 @@
 import { NaturalNumber } from '@modern-kit/types';
 
-export const chunk = <T>(array: T[], size: NaturalNumber<number>): any[][] => {
+export const chunk = <T, U extends number>(
+  array: T[],
+  size: NaturalNumber<U>
+): T[][] => {
   return array.reduce((result, _, index) => {
     if (index % size === 0) {
       return [...result, array.slice(index, index + size)];
