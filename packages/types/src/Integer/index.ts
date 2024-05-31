e61b7c08caa1a9aaab30613143fe21a17f52a708
Collection50 <fkaus65@naver.com>
2024-06-01 01:48:32 +0900

    fix: 불필요한 NaN 타입 제거

--- packages/types/src/Integer/index.ts
+++ packages/types/src/Integer/index.ts
@@ -1,5 +1,3 @@
-export type Integer<T extends number> = `${T}` extends
-  | `${string}.${string}`
-  | 'NaN'
+export type Integer<T extends number> = `${T}` extends `${string}.${string}`
   ? never
   : T;
