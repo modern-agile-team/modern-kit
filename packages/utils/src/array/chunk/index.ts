c48fcf08ba694804a541cabd8c9d2a1723e7f9f5
Collection50 <fkaus65@naver.com>
2024-05-31 20:07:12 +0900

    fix: array.length, size가 0인 경우, size >= array.length인 경우의 early return 구문 추가

--- packages/utils/src/array/chunk/index.ts
+++ packages/utils/src/array/chunk/index.ts
@@ -4,6 +4,14 @@ export const chunk = <T, U extends number>(
   array: T[],
   size: NaturalNumber<U>
 ): T[][] => {
+  if (array.length === 0 || size === 0) {
+    return [];
+  }
+
+  if (size >= array.length) {
+    return [array];
+  }
+
   return array.reduce((result, _, index) => {
     if (index % size === 0) {
       return [...result, array.slice(index, index + size)];
