ddd82027b21b51e3db33ac3059f86db52eab4cac
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-06-07 01:45:11 +0900

    revert(utils): delay callback 제거 (#207)

--- packages/utils/src/common/delay/index.ts
+++ packages/utils/src/common/delay/index.ts
@@ -1,12 +1,9 @@
-export const delay = (time: number, callback?: () => void): Promise<void> => {
+export const delay = (time: number): Promise<void> => {
   return new Promise<void>((resolve, reject) => {
     if (!Number.isInteger(time) || time < 0) {
       reject(new Error('Invalid time value'));
     }
 
-    setTimeout(() => {
-      if (callback) callback();
-      resolve();
-    }, time);
+    setTimeout(resolve, time);
   });
 };
