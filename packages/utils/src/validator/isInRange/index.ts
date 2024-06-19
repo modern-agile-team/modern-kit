b9277f0b99f9a04bc7bcd7ee243ad459c36a5eb6
Sangminnn <psm0102psm@gmail.com>
2024-06-20 00:24:21 +0900

    feat(utils): isInRange 함수 작업 완료 (#242)
    * feat(utils): isInRange 함수 작업 완료
    
    * imp(utils): isInRange 함수 default value 주입방식 변경 및 테스트케이스 일부 수정 완료
    
    * Create wise-jeans-provide.md

---
+++ packages/utils/src/validator/isInRange/index.ts
@@ -0,0 +1,31 @@
+interface IsInRangeProps {
+  value: number;
+  min: number;
+  max: number;
+  equalOptions?: {
+    min?: boolean;
+    max?: boolean;
+  };
+}
+
+export const isInRange = ({
+  value,
+  min,
+  max,
+  equalOptions = {},
+}: IsInRangeProps) => {
+  if (min == null || max == null) {
+    throw new Error('min and max values are invalid.');
+  }
+
+  if (min > max) {
+    throw new Error('min value cannot be greater than the max value.');
+  }
+
+  const { min: minEqual = true, max: maxEqual = false } = equalOptions;
+
+  const isWithinMin = minEqual ? value >= min : value > min;
+  const isWithinMax = maxEqual ? value <= max : value < max;
+
+  return isWithinMin && isWithinMax;
+};
---
+++ packages/utils/src/validator/isInRange/isInRange.spec.ts
@@ -0,0 +1,139 @@
+import { isInRange } from '.';
+
+describe('isInRange', () => {
+  it('should return true if value is in range', () => {
+    expect(
+      isInRange({
+        value: 5,
+        min: 0,
+        max: 10,
+      }),
+    ).toBe(true);
+  });
+
+  it('should return correct value if equalOptions min is true and max is false', () => {
+    expect(
+      isInRange({
+        value: 0,
+        min: 0,
+        max: 10,
+      }),
+    ).toBe(true);
+    expect(
+      isInRange({
+        value: 10,
+        min: 0,
+        max: 10,
+      }),
+    ).toBe(false);
+  });
+
+  it('should return correct value if equalOptions min is false and max is true', () => {
+    expect(
+      isInRange({
+        value: 0,
+        min: 0,
+        max: 10,
+        equalOptions: {
+          min: false,
+          max: true,
+        },
+      }),
+    ).toBe(false);
+    expect(
+      isInRange({
+        value: 10,
+        min: 0,
+        max: 10,
+        equalOptions: {
+          min: false,
+          max: true,
+        },
+      }),
+    ).toBe(true);
+  });
+
+  it('should return correct value if equalOptions min and max is false', () => {
+    expect(
+      isInRange({
+        value: 0,
+        min: 0,
+        max: 10,
+        equalOptions: {
+          min: false,
+        },
+      }),
+    ).toBe(false);
+    expect(
+      isInRange({
+        value: 10,
+        min: 0,
+        max: 10,
+        equalOptions: {
+          min: false,
+        },
+      }),
+    ).toBe(false);
+  });
+
+  it('should return correct value if equalOptions min and max is true', () => {
+    expect(
+      isInRange({
+        value: 0,
+        min: 0,
+        max: 10,
+        equalOptions: {
+          max: true,
+        },
+      }),
+    ).toBe(true);
+    expect(
+      isInRange({
+        value: 10,
+        min: 0,
+        max: 10,
+        equalOptions: {
+          max: true,
+        },
+      }),
+    ).toBe(true);
+  });
+
+  it('should return false if value is not in range', () => {
+    expect(
+      isInRange({
+        value: 15,
+        min: 0,
+        max: 10,
+      }),
+    ).toBe(false);
+  });
+
+  it('should throw error if min or max is invalid value', () => {
+    expect(() =>
+      isInRange({
+        value: 0,
+        min: null as unknown as number,
+        max: 10,
+      }),
+    ).toThrowError('min and max values are invalid.');
+
+    expect(() =>
+      isInRange({
+        value: 0,
+        min: 0,
+        max: null as unknown as number,
+      }),
+    ).toThrowError('min and max values are invalid.');
+  });
+
+  it('should throw error if min is greater than max', () => {
+    expect(() =>
+      isInRange({
+        value: 5,
+        min: 10,
+        max: 0,
+      }),
+    ).toThrowError('min value cannot be greater than the max value.');
+  });
+});
