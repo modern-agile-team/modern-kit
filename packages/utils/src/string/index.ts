36aed714628da4eb42141491f2b7eff28e73843b
ssi02014 <ssi02014@naver.com>
2024-06-20 00:59:53 +0900

    fix: export 위치

--- packages/utils/src/string/index.ts
+++ packages/utils/src/string/index.ts
@@ -1,5 +1,5 @@
 export * from './countSubstringOccurrences';
+export * from './extractLetters';
 export * from './extractNumber';
 export * from './removeSpecialCharacters';
 export * from './repeatCharacters';
-export * from './extractLetters';
