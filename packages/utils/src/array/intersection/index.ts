64d12a2f39bc81760f149a6953689b97fbcb8d05
sangminnn <psm0102psm@gmail.com>
2024-06-26 23:09:15 +0900

    chore(utils): intersection 내부구현 수정 완료

--- packages/utils/src/array/intersection/index.ts
+++ packages/utils/src/array/intersection/index.ts
@@ -1,25 +1,18 @@
 import { identity } from '../../common';
 
+import { intersectionWithDuplicates } from '../intersectionWithDuplicates';
+import { uniq } from '../uniq';
+
 export const intersection = <T, U = T>(
   firstArr: T[] | readonly T[],
   secondArr: T[] | readonly T[],
   iteratee: (item: T) => T | U = identity,
 ) => {
-  const appliedIterateeSecondSet = new Set(secondArr.map(iteratee));
+  const intersection = intersectionWithDuplicates(
+    firstArr,
+    secondArr,
+    iteratee,
+  );
 
-  const intersection = [];
-  const checkedSet = new Set();
-
-  for (const item of firstArr) {
-    const appliedIterateeFirstArrItem = iteratee(item);
-
-    if (checkedSet.has(appliedIterateeFirstArrItem)) continue;
-
-    if (appliedIterateeSecondSet.has(appliedIterateeFirstArrItem)) {
-      intersection.push(item);
-      checkedSet.add(appliedIterateeFirstArrItem);
-    }
-  }
-
-  return intersection;
+  return uniq(intersection, iteratee);
 };
