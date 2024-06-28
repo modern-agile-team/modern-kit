ea6a954e7d9152ff1e8533de0fab007e82ee5f22
ssi02014 <ssi02014@naver.com>
2024-06-28 19:36:10 +0900

    fix: isFunction 추가

--- packages/react/src/hooks/useMergeRefs/index.ts
+++ packages/react/src/hooks/useMergeRefs/index.ts
@@ -1,9 +1,10 @@
+import { isFunction } from '@modern-kit/utils';
 import React, { useCallback } from 'react';
 
 const mergeRefs = <T = unknown>(...refs: React.Ref<T>[]) => {
   return (node: T) =>
     refs.forEach((ref) => {
-      if (typeof ref === 'function') {
+      if (isFunction(ref)) {
         ref(node);
       } else if (ref != null) {
         const refToUse = ref as React.MutableRefObject<T>;
