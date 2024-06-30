ee8e7078e91e5884df66109d066fb39772443884
ssi02014 <ssi02014@naver.com>
2024-06-30 18:29:13 +0900

    feat(react): useStepState storage 옵션 추가

--- packages/utils/src/storage/getStorageItem/index.ts
+++ packages/utils/src/storage/getStorageItem/index.ts
@@ -2,28 +2,27 @@ import { StorageType } from '../types';
 import { isClient } from '../../device';
 import { isFunction } from '../../validator/isFunction';
 import { Nullable } from '@modern-kit/types';
+import { parseJSON } from '../../common';
 
 export const getStorageItem = <T>(
   type: StorageType,
   key: string,
   initialValue: Nullable<T | (() => T)> = null
 ) => {
-  const initialValueToUse = isFunction(initialValue)
-    ? initialValue()
-    : initialValue;
-
   if (!isClient()) {
-    console.error('Cannot be executed unless it is a client environment.');
-    return initialValueToUse;
+    throw new Error('Cannot be executed unless it is a client environment.');
   }
 
   try {
+    const initialValueToUse = isFunction(initialValue)
+      ? initialValue()
+      : initialValue;
+
     const storage = window[type];
     const storedValue = storage.getItem(key);
 
-    return storedValue ? JSON.parse(storedValue) : initialValueToUse;
-  } catch (error) {
-    console.error(`Error reading ${type} key "${key}"`);
-    return initialValueToUse;
+    return storedValue ? parseJSON(storedValue) : initialValueToUse;
+  } catch (err) {
+    throw new Error(`Failed to read data for key "${key}" in ${type}: ${err}`);
   }
 };
