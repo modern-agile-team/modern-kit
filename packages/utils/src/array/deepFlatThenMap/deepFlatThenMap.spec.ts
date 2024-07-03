84a08275c18a654a2541ea133f657b04f28c4670
ssi02014 <ssi02014@naver.com>
2024-07-04 03:01:05 +0900

    test(utils): deepFlatThenMap 테스트 코드 개선

--- packages/utils/src/array/deepFlatThenMap/deepFlatThenMap.spec.ts
+++ packages/utils/src/array/deepFlatThenMap/deepFlatThenMap.spec.ts
@@ -2,12 +2,20 @@ import { deepFlatThenMap } from '.';
 
 describe('deepFlatThenMap', () => {
   const arr = [1, 2, [3, 4, [5, 6]]];
+  const mixedArr = [1, 2, ['123', 4, [true, 6]]];
 
   it('should flatten the array', () => {
     const flattenArray = deepFlatThenMap(arr);
 
     expect(flattenArray).toEqual([1, 2, 3, 4, 5, 6]);
     expectTypeOf(flattenArray).toEqualTypeOf<number[]>();
+
+    const flattenMixedArray = deepFlatThenMap(mixedArr);
+
+    expect(flattenMixedArray).toEqual([1, 2, '123', 4, true, 6]);
+    expectTypeOf(flattenMixedArray).toEqualTypeOf<
+      (string | number | boolean)[]
+    >();
   });
 
   it('should apply the iteratee function to each element and flatten', () => {
