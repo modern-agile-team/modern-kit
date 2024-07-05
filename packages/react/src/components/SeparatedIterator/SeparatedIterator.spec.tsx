6e186313130333470d46d44f046da6cea7405086
ssi02014 <ssi02014@naver.com>
2024-07-05 18:46:44 +0900

    fix(react): 컴포넌트 개선 Iterator, SeparatedIterator

--- packages/react/src/components/SeparatedIterator/SeparatedIterator.spec.tsx
+++ packages/react/src/components/SeparatedIterator/SeparatedIterator.spec.tsx
@@ -30,6 +30,23 @@ describe('SeparatedIterator', () => {
     expect(separators).toHaveLength(testItems.length - 1);
   });
 
+  it('should render the correct number of items and separators at the specified interval', () => {
+    renderSetup(
+      <SeparatedIterator
+        items={testItems}
+        renderItem={(item) => <div>{item.name}</div>}
+        separatorInterval={2}
+        separator={<div>separator</div>}
+      />
+    );
+
+    const renderItems = screen.getAllByText(/Item \d/);
+    const separators = screen.getAllByText(/separator/);
+
+    expect(renderItems).toHaveLength(testItems.length);
+    expect(separators).toHaveLength(1);
+  });
+
   it('should render the correct number of separators when includeLastSeparator is true', () => {
     renderSetup(
       <SeparatedIterator
@@ -45,5 +62,22 @@ describe('SeparatedIterator', () => {
 
     expect(renderItems).toHaveLength(testItems.length);
     expect(separators).toHaveLength(testItems.length);
+  });
+
+  it('should not render any items when items prop is undefined', () => {
+    renderSetup(
+      <SeparatedIterator
+        items={undefined}
+        renderItem={() => <div>Item</div>}
+        separator={<div>separator</div>}
+        includeLastSeparator
+      />
+    );
+
+    const renderItems = screen.queryAllByText(/Item \d/);
+    const separators = screen.queryAllByText(/separator/);
+
+    expect(renderItems).toHaveLength(0);
+    expect(separators).toHaveLength(0);
   });
 });
