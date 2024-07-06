cf901f78bec86aad67bb157fbf64696672c64bea
JongHyeon Kim (Bell) <woaol@naver.com>
2024-07-06 17:36:39 +0900

    feat(utils): isSubset 의 배열 요소가 참조형인 경우를 위한 deepEqual 적용

--- packages/utils/src/validator/isSubset/index.ts
+++ packages/utils/src/validator/isSubset/index.ts
@@ -1,16 +1,29 @@
+import { deepEqual } from '../../common/deepEqual';
+
+const goDeepEqual = <T, U>(
+  baseArray: readonly T[] | U[],
+  cmpArray: readonly T[] | U[]
+) => {
+  return (cmpArray as any).every((cObj) =>
+    baseArray.some((bObj) => deepEqual(bObj, cObj))
+  );
+};
+
 export const isSubset = <T, U>(
-  parentArray: T[] | readonly T[],
-  childArray: T[] | readonly T[],
+  parentArray: readonly T[],
+  childArray: readonly T[],
   iteratee?: (item: T) => U
 ) => {
   const baseArray = iteratee ? parentArray.map(iteratee) : parentArray;
+  const cmpArray = iteratee ? childArray.map(iteratee) : childArray;
 
+  // 배열 요소가 참조형인 경우
+  if (typeof baseArray[0] === 'object') {
+    return goDeepEqual(baseArray, cmpArray);
+  }
+
+  // 배열 요소가 원시값인 경우 (Set 적용)
   const baseSet = new Set<T | U>(baseArray);
 
-  for (const el of childArray) {
-    if (!baseSet.has(iteratee ? iteratee(el) : el)) {
-      return false;
-    }
-  }
-  return true;
+  return (cmpArray as any).every((el) => baseSet.has(el));
 };
