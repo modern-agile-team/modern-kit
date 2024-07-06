65e0fefcdfdd2b0762f597d92d4b7bf7b620a97f
JongHyeon Kim (Bell) <woaol@naver.com>
2024-07-06 17:39:06 +0900

    test(utils): isSubset deepEqual 적용에 대한 테스트 재조정

--- packages/utils/src/validator/isSubset/isSubset.spec.ts
+++ packages/utils/src/validator/isSubset/isSubset.spec.ts
@@ -19,38 +19,87 @@ describe('isSubset', () => {
     expect(isSubset(parentArray, childArray2)).toBeFalsy();
   });
 
-  it('if elements type is reference', () => {
+  it('if elements type is array', () => {
     const parentArray = [
+      [0, 1, 2, 3, 4],
+      [2, 3, 4, 5, 6],
+    ];
+    const childArray1 = [[0, 1, 2, 3, 4]];
+    const childArray2 = [[2, 3, 4, 5]];
+
+    expect(isSubset(parentArray, childArray1)).toBeTruthy();
+    expect(isSubset(parentArray, childArray2)).toBeFalsy();
+    expect(isSubset(parentArray, childArray1, (obj) => obj[0])).toBeTruthy();
+    expect(isSubset(parentArray, childArray2, (obj) => obj[0])).toBeTruthy();
+  });
+
+  it('if elements type is reference', () => {
+    type user = {
+      name: string;
+      age: number;
+      bestFriend: {
+        name: string;
+        age: number;
+      };
+      friends: string[];
+    };
+
+    const parentArray: user[] = [
       {
         name: 'Peter',
         age: 13,
+        bestFriend: {
+          name: 'Aimee',
+          age: 25,
+        },
+        friends: ['Charlie', 'Chuck', 'Catherine'],
       },
       {
         name: 'Aimee',
         age: 25,
-      },
-    ];
-    const childArray1 = [
-      {
-        name: 'Peter',
-        age: 13,
-      },
-    ];
-    const childArray2 = [
-      {
-        name: 'Aimee',
-        age: 13,
+        bestFriend: {
+          name: 'Charlie',
+          age: 19,
+        },
+        friends: ['Peter', 'Chuck', 'Catherine'],
       },
     ];
 
-    expect(isSubset(parentArray, childArray1)).toBeFalsy();
+    // parentArray[1] 과 동일 요소를 가짐.
+    const childArray1: user[] = [
+      {
+        name: 'Aimee',
+        age: 25,
+        bestFriend: {
+          name: 'Charlie',
+          age: 19,
+        },
+        friends: ['Peter', 'Chuck', 'Catherine'],
+      },
+    ];
+
+    // parentArray[0]의 friends의 마지막 요소가 다름.
+    const childArray2: user[] = [
+      {
+        name: 'Peter',
+        age: 13,
+        bestFriend: {
+          name: 'Aimee',
+          age: 25,
+        },
+        friends: ['Charlie', 'Chuck', 'bell'],
+      },
+    ];
+
+    expect(isSubset(parentArray, childArray1)).toBeTruthy();
+    expect(isSubset(parentArray, childArray2)).toBeFalsy();
     expect(
-      isSubset(parentArray, childArray1, (obj) => JSON.stringify(obj))
+      isSubset(parentArray, childArray2, (obj: user) => {
+        return {
+          name: obj.name,
+          bestFriend: obj.bestFriend,
+        };
+      })
     ).toBeTruthy();
-    expect(
-      isSubset(parentArray, childArray2, (obj) => JSON.stringify(obj))
-    ).toBeFalsy();
-    expect(isSubset(parentArray, childArray2, (obj) => obj.name)).toBeTruthy(); // 이름만 비교
-    expect(isSubset(parentArray, childArray2, (obj) => obj.age)).toBeTruthy(); // 나이만 비교
   });
 });
