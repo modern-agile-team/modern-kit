53144df156bbc8e92515c7166a1553020f0808d1
JongHyeon Kim (Bell) <woaol@naver.com>
2024-07-07 22:41:57 +0900

    feat(utils): deepEqual 코드 삭제. (부분집합의 정의를 사용자에게 위임)

--- packages/utils/src/validator/isSubset/index.ts
+++ packages/utils/src/validator/isSubset/index.ts
@@ -1,14 +1,3 @@
-import { deepEqual } from '../../common/deepEqual';
-
-const goDeepEqual = <T, U>(
-  baseArray: readonly T[] | U[],
-  cmpArray: readonly T[] | U[]
-) => {
-  return (cmpArray as any).every((cObj: T | U) =>
-    baseArray.some((bObj: T | U) => deepEqual(bObj, cObj))
-  );
-};
-
 export const isSubset = <T, U>(
   parentArray: readonly T[],
   childArray: readonly T[],
@@ -17,12 +6,6 @@ export const isSubset = <T, U>(
   const baseArray = iteratee ? parentArray.map(iteratee) : parentArray;
   const cmpArray = iteratee ? childArray.map(iteratee) : childArray;
 
-  // 배열 요소가 참조형인 경우
-  if (typeof baseArray[0] === 'object') {
-    return goDeepEqual(baseArray, cmpArray);
-  }
-
-  // 배열 요소가 원시값인 경우 (Set 적용)
   const baseSet = new Set<T | U>(baseArray);
 
   return (cmpArray as any).every((el: T | U) => baseSet.has(el));
