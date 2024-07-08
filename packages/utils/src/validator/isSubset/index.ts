c473786ca3172f78f661a9383a774f3dc18cf1c8
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-07-08 14:26:06 +0900

    Update packages/utils/src/validator/isSubset/index.ts

--- packages/utils/src/validator/isSubset/index.ts
+++ packages/utils/src/validator/isSubset/index.ts
@@ -2,7 +2,7 @@ import { difference } from '../../array';
 
 export const isSubset = <T, U>(
   superset: T[] | readonly T[],
-  subset: readonly T[],
+  subset: T[] | readonly T[],
   iteratee?: (item: T) => U
 ) => {
   return difference(subset, superset, iteratee).length === 0;
