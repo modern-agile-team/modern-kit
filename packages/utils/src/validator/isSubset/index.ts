a9439fb6b31756a23ebf9514dc7debff80dbc3cb
JongHyeon Kim (Bell) <woaol@naver.com>
2024-07-08 13:56:19 +0900

    refactor(utils): isSubset 함수 재구현 (utils/array/difference 재사용)

--- packages/utils/src/validator/isSubset/index.ts
+++ packages/utils/src/validator/isSubset/index.ts
@@ -1,12 +1,9 @@
+import { difference } from '../../array';
+
 export const isSubset = <T, U>(
-  parentArray: readonly T[],
-  childArray: readonly T[],
+  superset: T[] | readonly T[],
+  subset: readonly T[],
   iteratee?: (item: T) => U
 ) => {
-  const baseArray = iteratee ? parentArray.map(iteratee) : parentArray;
-  const cmpArray = iteratee ? childArray.map(iteratee) : childArray;
-
-  const baseSet = new Set<T | U>(baseArray);
-
-  return (cmpArray as any).every((el: T | U) => baseSet.has(el));
+  return difference(subset, superset, iteratee).length === 0;
 };
