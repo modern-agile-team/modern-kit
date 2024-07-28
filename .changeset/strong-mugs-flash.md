f8058778b65525ebc4d6cc15395ca0abb5fe3727
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-07-28 15:59:57 +0900

    feat(react): useScrollTo 기능 변경 및 isWindow 함수 추가 (#376)

---
+++ .changeset/strong-mugs-flash.md
@@ -0,0 +1,5 @@
+---
+'@modern-kit/utils': minor
+---
+
+feat(utils): isWindow 신규 함수 추가 - @ssi02014
--- packages/react/src/hooks/useScrollTo/index.ts
+++ packages/react/src/hooks/useScrollTo/index.ts
@@ -1,47 +1,91 @@
-import { usePreservedState } from '../usePreservedState';
-import { useIsomorphicLayoutEffect } from '../useIsomorphicLayoutEffect';
-import { useCallback, useRef } from 'react';
+import { useCallback, useEffect, useRef } from 'react';
+import { ScrollToElementOptions, getRelativePosition } from './internal';
 
-export function useScrollTo(autoScrollOptions?: ScrollToOptions): {
-  ref: React.MutableRefObject<Window | null>;
-  scrollTo: (scrollToOptions?: ScrollToOptions) => void;
+/**
+ * @description 스크롤 기능을 제공하는 커스텀 훅입니다.
+ * 훅이 반환하는 `containerRef`를 특정 엘리먼트에 할당하면 해당 요소를 스크롤 합니다.
+ * `containerRef`를 할당하지 않으면 `window`를 스크롤합니다.
+ *
+ * @returns 스크롤 요소를 지정 할 `containerRef`와 스크롤을 동작 시킬 `scrollToPosition`, `scrollToElement` 함수를 포함하는 객체입니다.
+ *
+ * @example
+ * // scrollToPosition를 사용하면 top, left와 같은 포지션 값을 기준으로 이동합니다.
+ * const { containerRef, scrollToPosition } = useScrollTo();
+ * scrollToPosition({ top, left, behavior });
+ *
+ * @example
+ * // scrollToElement를 사용하면 인자로 넣은 엘리먼트를 기준으로 이동합니다.
+ * const { containerRef, scrollToElement } = useScrollTo();
+ * scrollToElement(target.current, { offsetX, offsetY });
+ */
+export function useScrollTo(): {
+  containerRef: React.MutableRefObject<Window | null>;
+  scrollToPosition: (scrollToOptions?: ScrollToOptions) => void;
+  scrollToElement: <E extends HTMLElement>(
+    target: E,
+    scrollToElementOptions?: ScrollToElementOptions
+  ) => void;
 };
 
-export function useScrollTo<T extends HTMLElement>(
-  autoScrollOptions?: ScrollToOptions
-): {
-  ref: React.RefObject<T>;
-  scrollTo: (scrollToOptions?: ScrollToOptions) => void;
+export function useScrollTo<T extends HTMLElement>(): {
+  containerRef: React.RefObject<T>;
+  scrollToPosition: (scrollToOptions?: ScrollToOptions) => void;
+  scrollToElement: <E extends HTMLElement>(
+    target: E,
+    scrollToElementOptions?: ScrollToElementOptions
+  ) => void;
 };
 
-export function useScrollTo<T extends HTMLElement>(
-  autoScrollOptions?: ScrollToOptions
-) {
-  const preservedAutoScrollOptions = usePreservedState(autoScrollOptions);
-  const ref = useRef<T | Window | null>(null);
+export function useScrollTo<T extends HTMLElement>() {
+  const containerRef = useRef<T | Window | null>(null);
 
-  const scrollTo = useCallback((scrollToOptions: ScrollToOptions = {}) => {
-    if (!ref.current) return;
+  const scrollToPosition = useCallback(
+    (scrollToOptions: ScrollToOptions = {}) => {
+      if (!containerRef.current) return;
 
-    const { left = 0, top = 0, behavior = 'auto' } = scrollToOptions;
-    const targetElement = ref.current;
+      const { left = 0, top = 0, behavior = 'auto' } = scrollToOptions;
 
-    targetElement.scrollTo({
-      left,
-      top,
-      behavior,
-    });
+      const scrollElement = containerRef.current;
+
+      scrollElement.scrollTo({
+        left,
+        top,
+        behavior,
+      });
+    },
+    []
+  );
+
+  const scrollToElement = useCallback(
+    <E extends HTMLElement>(
+      target: E,
+      scrollToElementOptions: ScrollToElementOptions = {}
+    ) => {
+      if (!containerRef.current) return;
+
+      const scrollElement = containerRef.current;
+      const { behavior = 'auto' } = scrollToElementOptions;
+
+      const { left, top } = getRelativePosition(
+        scrollElement,
+        target,
+        scrollToElementOptions
+      );
+
+      scrollElement.scrollTo({
+        top,
+        left,
+        behavior,
+      });
+    },
+    []
+  );
+
+  useEffect(() => {
+    if (!containerRef.current) {
+      containerRef.current = window;
+    }
   }, []);
 
-  useIsomorphicLayoutEffect(() => {
-    if (!ref.current) {
-      ref.current = window;
-    }
-
-    if (preservedAutoScrollOptions) {
-      scrollTo(preservedAutoScrollOptions);
-    }
-  }, [preservedAutoScrollOptions, scrollTo]);
-
-  return { ref, scrollTo };
+  return { containerRef, scrollToPosition, scrollToElement };
 }
