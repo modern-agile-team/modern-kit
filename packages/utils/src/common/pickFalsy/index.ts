85520464f0c24a257f29c49b696fe65e29cb9524
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-07-30 15:10:42 +0900

    Update packages/utils/src/common/pickFalsy/index.ts

--- packages/utils/src/common/pickFalsy/index.ts
+++ packages/utils/src/common/pickFalsy/index.ts
@@ -5,9 +5,11 @@ import {
 } from './internal';
 
 /**
- * @description 선택한 타입에 대한 falsy 체크함수를 반환합니다.
+ * @description 선택한 타입에 대한 falsy 체크 함수를 반환합니다.
+ * 
+ * 필요하다면 숫자 0, 빈 문자열(""), 빈 객체({}), 빈 배열([])에 대해 판단할 수 있습니다.
  *
- * 인자가 없더라도 기본적으로 null, undefined에 대한 체크를 수행합니다.
+ * 인자가 없다면, 기본적으로 `null`, `undefined`에 대한 falsy 값 체크를 수행합니다.
  *
  * @param arr - falsy를 체크하고자 하는 타입을 받습니다.
  * @returns - 선택한 타입에 대한 falsy함수를 반환합니다. falsy를 체크하는 함수이기때문에 해당 함수로부터의 반환값이 true라면 falsy, false라면 truthy입니다.
