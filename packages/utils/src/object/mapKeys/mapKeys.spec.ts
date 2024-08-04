89f614f2d859ecab3adc8612db1a9313efa6f631
ssi02014 <ssi02014@naver.com>
2024-08-04 15:10:23 +0900

    test: 누락 import 추가

--- packages/utils/src/object/mapKeys/mapKeys.spec.ts
+++ packages/utils/src/object/mapKeys/mapKeys.spec.ts
@@ -1,3 +1,4 @@
+import { describe, it, expect } from 'vitest';
 import { mapKeys } from '.';
 
 describe('mapKeys', () => {
@@ -10,26 +11,19 @@ describe('mapKeys', () => {
 
   it('should map object keys to uppercase', () => {
     const obj = { a: 'apple', b: 'banana', c: 'cherry' } as const;
-    const iteratee = ({
-      key,
-    }: {
-      key: string | number;
-      value: string;
-    }): string => key.toString().toUpperCase();
     const expected = { A: 'apple', B: 'banana', C: 'cherry' } as const;
 
-    const result = mapKeys(obj, iteratee);
+    const result = mapKeys(obj, ({ key }) => key.toString().toUpperCase());
 
     expect(result).toEqual(expected);
   });
 
   it('should return an empty object for an empty input object', () => {
     const obj = {} as const;
-    const iteratee = ({ key }: { key: string | number; value: any }): string =>
-      key.toString();
+
     const expected = {} as const;
 
-    const result = mapKeys(obj, iteratee);
+    const result = mapKeys(obj, ({ key }: { key: string }) => key.toString());
 
     expect(result).toEqual(expected);
   });
