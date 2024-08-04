98f8d57bd4b2aa1344af9726368a02aa911564b8
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-08-04 23:34:45 +0900

    fix: mapKeys, Values 로직 일부 수정 (#398)

--- packages/utils/src/object/mapKeys/index.ts
+++ packages/utils/src/object/mapKeys/index.ts
@@ -1,5 +1,3 @@
-import { hasProperty } from '../../validator';
-
 /**
  * @description 주어진 객체의 각 키에 대해 제공된 변환 함수를 호출하여 새 객체를 생성하는 함수입니다.
  *
@@ -8,6 +6,11 @@ import { hasProperty } from '../../validator';
  * @param {T} object - 순회할 원본 객체입니다.
  * @param {(iterateData: { key: keyof T; value: T[keyof T]; object: T }) => U} iteratee - 객체의 각 키에 대해 호출할 변환 함수입니다.
  * @returns {Record<U, T[keyof T]>} 변환 함수의 결과를 포함하는 새 객체를 반환합니다.
+ *
+ * @example
+ * const obj = { a: 1, b: 2, c: 3 };
+ * mapKeys(obj, ({ key, value }) => key + value);
+ * // { a1: 1, b2: 2, c3: 3 }
  */
 export function mapKeys<
   T extends Record<PropertyKey, any>,
@@ -17,12 +20,13 @@ export function mapKeys<
   iteratee: (iterateData: { key: keyof T; value: T[keyof T]; object: T }) => U
 ): Record<U, T[keyof T]> {
   const result = {} as Record<U, T[keyof T]>;
+  const keys = Object.keys(object);
 
-  for (const key in object) {
-    if (hasProperty(object, key)) {
-      const newKey = iteratee({ key, value: object[key], object });
-      result[newKey] = object[key];
-    }
+  for (const key of keys) {
+    const value = object[key] as T[keyof T];
+    const newKey = iteratee({ key, value, object });
+
+    result[newKey] = value;
   }
 
   return result;
--- packages/utils/src/object/mapValues/index.ts
+++ packages/utils/src/object/mapValues/index.ts
@@ -1,5 +1,3 @@
-import { hasProperty } from '../../validator';
-
 /**
  * @description 주어진 객체의 각 값에 대해 제공된 변환 함수를 호출하여 새 객체를 생성하는 함수입니다.
  *
@@ -8,17 +6,22 @@ import { hasProperty } from '../../validator';
  * @param {T} object - 순회할 원본 객체입니다.
  * @param {(iterateData: { key: keyof T; value: T[keyof T]; object: T }) => V} iteratee - 객체의 각 값에 대해 호출할 변환 함수입니다.
  * @returns {Record<keyof T, V>} 변환 함수의 결과를 포함하는 새 객체를 반환합니다.
+ *
+ * @example
+ * const obj = { fred: { age: 40 }, pebbles: { age: 1 } };
+ * mapValues(users, ({ value }) => value.age)
+ * // { fred: 40, pebbles: 1 }
  */
 export function mapValues<T extends Record<PropertyKey, any>, V>(
   object: T,
   iteratee: (iterateData: { key: keyof T; value: T[keyof T]; object: T }) => V
 ): Record<keyof T, V> {
   const result = {} as Record<keyof T, V>;
+  const keys = Object.keys(object);
 
-  for (const key in object) {
-    if (hasProperty(object, key)) {
-      result[key] = iteratee({ key, value: object[key], object });
-    }
+  for (const key of keys) {
+    const value = object[key] as T[keyof T];
+    result[key as keyof T] = iteratee({ key, value, object });
   }
 
   return result;
