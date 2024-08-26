011cd65a79137d257bf0c171625ddf9c275d827d
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-08-26 16:36:03 +0900

    Update packages/utils/src/array/compact/index.ts

--- packages/utils/src/array/compact/index.ts
+++ packages/utils/src/array/compact/index.ts
@@ -3,10 +3,10 @@ type Retained<T> = Exclude<T, Removable>;
 /**
  * @description 배열에서 falsy 값을 제거합니다.
  *
- * false, 0, '', null, undefined 값을 제거하고 새로운 배열을 반환합니다.
+ * `false`, `0`, `''`, `null`, `undefined`, `NaN`값을 제거하고 새로운 배열을 반환합니다.
  *
  * @template T - 배열 요소의 유형입니다.
- * @param {readonly T[]} arr - falsy 값을 제거할 배열입니다.
+ * @param {T[] | readonly T[]} arr - falsy 값을 제거할 배열입니다.
  * @returns {Retained<T>[]} falsy 값이 제거된 새로운 배열을 반환합니다.
  *
  * @example
@@ -15,7 +15,7 @@ type Retained<T> = Exclude<T, Removable>;
  * compact(arr);
  * // [1, 2, 3, 4, 5]
  */
-export function compact<T>(arr: readonly T[]): Retained<T>[] {
+export function compact<T>(arr: T[] | readonly T[]): Retained<T>[] {
   const result: Retained<T>[] = [];
 
   for (const item of arr) {
