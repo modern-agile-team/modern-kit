a07f462acade6bcd9061294303111c75bf950067
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-09-27 18:26:50 +0900

    fix(utils): contains 타입 개선 (#488)
    * fix(utils): contains 타입 개선
    
    * revert: contains revert

--- packages/utils/src/array/contains/index.ts
+++ packages/utils/src/array/contains/index.ts
@@ -1,3 +1,28 @@
+/**
+ * 주어진 배열에 특정 값이 포함되어 있는지 확인하는 함수입니다.
+ * 기본적으로 `Object.is`를 사용하여 비교를 수행하며, 사용자 정의 비교 함수(comparator)를 사용할 수 있습니다.
+ *
+ * @template T - 배열의 요소 타입입니다.
+ * @param {T[] | readonly T[]} arr - 검색할 배열입니다. 변경 불가능한 읽기 전용 배열도 허용됩니다.
+ * @param {U} value - 배열에서 찾고자 하는 값입니다.
+ * @param {(x: T, y: U) => boolean} [comparator=Object.is] - 배열의 요소와 찾고자 하는 값을 비교할 때 사용할 사용자 정의 비교 함수입니다. 기본값은 `Object.is`입니다.
+ * @returns {value is U} 주어진 값이 배열에 포함되어 있으면 `true`를, 그렇지 않으면 `false`를 반환합니다.
+ *
+ * @example
+ * // 기본 비교 함수 사용 (Object.is)
+ * const numbers = [1, 2, 3];
+ *
+ * contains(numbers, 2); // true
+ * contains(numbers, 4); // false
+ *
+ * @example
+ * // 사용자 정의 비교 함수 사용
+ * const objects = [{ id: 1 }, { id: 2 }];
+ * const comparator = (a: { id: number }, b: { id: number }) => a.id === b.id;
+ *
+ * contains(objects, { id: 2 }, (a, b) => a.id === b.id); // true
+ * contains(objects, { id: 3 }, (a, b) => a.id === b.id); // false
+ */
 export function contains<T>(
   arr: T[] | readonly T[],
   value: unknown,
--- packages/utils/src/array/difference/index.ts
+++ packages/utils/src/array/difference/index.ts
@@ -1,8 +1,38 @@
+/**
+ * 첫 번째 배열에서 두 번째 배열에 없는 요소들을 반환하는 함수입니다.
+ * 선택적으로 제공된 `iteratee` 함수를 사용하여 각 요소를 변환한 후 비교할 수 있습니다.
+ *
+ * @template T - 첫 번째 배열의 요소 타입입니다.
+ * @template U - `iteratee` 함수가 반환하는 타입으로, 기본값은 `T`와 같습니다.
+ * @param {T[] | readonly T[]} firstArr - 차집합을 구할 기준이 되는 첫 번째 배열입니다.
+ * @param {T[] | readonly T[]} secondArr - 첫 번째 배열에서 제거할 요소들이 포함된 두 번째 배열입니다.
+ * @param {(item: T) => U} [iteratee] - 각 배열 요소를 비교하기 전에 변환하는 함수입니다. 이 함수가 제공되면, 변환된 결과를 기준으로 비교합니다.
+ * @returns {T[]} 두 번째 배열에 없는 첫 번째 배열의 요소들이 담긴 배열을 반환합니다.
+ *
+ * @example
+ * // 기본 사용 예제
+ * const arr1 = [1, 2, 3, 4];
+ * const arr2 = [2, 4];
+ *
+ * difference(arr1, arr2); // [1, 3]
+ *
+ * @example
+ * // iteratee 함수 사용 예제
+ * const users = [
+ *   { id: 1, name: 'Alice' },
+ *   { id: 2, name: 'Bob' },
+ *   { id: 3, name: 'Charlie' }
+ * ];
+ * const activeUserIds = [1, 3];
+ *
+ * difference(users, activeUserIds, user => user.id));
+ * // [{ id: 2, name: 'Bob' }]
+ */
 export function difference<T, U = T>(
   firstArr: T[] | readonly T[],
   secondArr: T[] | readonly T[],
   iteratee?: (item: T) => U
-) {
+): T[] {
   const result = [];
   const secondSet = new Set<T | U>(
     iteratee ? secondArr.map(iteratee) : secondArr
