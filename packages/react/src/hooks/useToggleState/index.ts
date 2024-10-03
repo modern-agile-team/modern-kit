fa9acbf12db5be569a89f495dc748496184967d6
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-10-03 20:40:47 +0900

    Update packages/react/src/hooks/useToggleState/index.ts

--- packages/react/src/hooks/useToggleState/index.ts
+++ packages/react/src/hooks/useToggleState/index.ts
@@ -1,11 +1,12 @@
 import { useCallback, useState } from 'react';
 /**
- *
- * @param value1 {T}
- * @param value2 {T}
- * @returns {[T, () => void]}
  * @description 두 개의 값을 토글하는 기능을 제공하는 훅입니다.
  * 기본값으로 제공된 두 개의 값을 관리하며, 선택된 값과 두 값을 토글할 수 있는 함수를 반환합니다.
+ *
+ * @template T - 토글할 값의 타입을 나타냅니다.
+ * @param {T} value1 - 초기값이자 첫 번째 토글 값입니다.
+ * @param {T} value2 - 두 번째 토글 값입니다.
+ * @returns {[T, () => void]}
  *
  * @example
  * const [value, toggle] = useToggle('ON', 'OFF');
@@ -13,7 +14,6 @@ import { useCallback, useState } from 'react';
  * value; // 'ON'
  * toggle();
  * value; // 'OFF'
- *
  */
 export function useToggleState<T>(value1: T, value2: T): [T, () => void] {
   const [value, setValue] = useState(value1);
