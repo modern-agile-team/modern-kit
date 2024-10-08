7adb27371bf742312151a84147decc0b7179bff3
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-10-08 16:02:35 +0900

    Update packages/utils/src/array/xor/index.ts

--- packages/utils/src/array/xor/index.ts
+++ packages/utils/src/array/xor/index.ts
@@ -12,7 +12,18 @@ import { intersection } from '../intersection';
  * @param {T[] | readonly T[]} arr2 - 두 번째 입력 배열입니다.
  * @param {(item: T) => U} [iteratee] - 선택적 반복자 함수입니다.
  * 제공된 경우, 각 요소에 대해 함수를 호출하여 비교 키를 생성합니다.
+ *
  * @returns {T[]} 두 배열의 대칭 차집합(XOR)을 포함하는 새 배열을 반환합니다.
+ *
+ * @example
+ * xor([1, 2], [2, 3]); // [1, 3]
+ *
+ * @example
+ * xor(
+ *  [{ id: 1 }, { id: 2 }],
+ *  [{ id: 2 }, { id: 3 }],
+ *  (x) => x.id
+ * ); // [{ id: 1 }, id: 3 }]
  */
 export function xor<T, U>(
   arr1: T[] | readonly T[],
