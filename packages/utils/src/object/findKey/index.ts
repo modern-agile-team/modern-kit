7c86fc17848800a357e7bb801156f0eb325090ec
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-10-17 18:01:05 +0900

    Update packages/utils/src/object/findKey/index.ts

--- packages/utils/src/object/findKey/index.ts
+++ packages/utils/src/object/findKey/index.ts
@@ -6,7 +6,6 @@
  * @returns {string | undefined} - 검색하고자 하는 조건에 부합하는 key를 반환합니다. 만약 조건에 부합하는 key가 없다면 undefined를 반환합니다.
  *
  * @example
- *
  * const obj = {
  *   bike: { active: true },
  *   plane: { active: true },
@@ -15,7 +14,6 @@
  *
  *  findKey(obj, (item) => item.active); // 'bike'
  */
-
 export function findKey<T extends Record<PropertyKey, any>>(
   obj: T,
   condition: (value: T[keyof T]) => boolean,
