53f35bfbc4fe5cea9a64379e4daa5f05fcc5cadd
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-10-17 18:11:12 +0900

    Update packages/utils/src/object/findKey/index.ts

--- packages/utils/src/object/findKey/index.ts
+++ packages/utils/src/object/findKey/index.ts
@@ -1,9 +1,10 @@
 /**
  * @description 객체에서 조건에 부합하는 key를 반환합니다.
  *
- * @param obj - 검색하고자 하는 객체입니다.
- * @param condition - 검색하고자 하는 조건입니다.
- * @returns {string | undefined} - 검색하고자 하는 조건에 부합하는 key를 반환합니다. 만약 조건에 부합하는 key가 없다면 undefined를 반환합니다.
+ * @template T - 키를 찾고자 하는 객체 요소의 타입
+ * @param {T} obj - 검색하고자 하는 객체입니다.
+ * @param {(value: T[keyof T]) => boolean} condition - 검색하고자 하는 조건입니다.
+ * @returns {string | undefined} 검색하고자 하는 조건에 부합하는 key를 반환합니다. 만약 조건에 부합하는 key가 없다면 undefined를 반환합니다.
  *
  * @example
  * const obj = {
@@ -17,7 +18,7 @@
 export function findKey<T extends Record<PropertyKey, any>>(
   obj: T,
   condition: (value: T[keyof T]) => boolean,
-) {
+): string | undefined {
   const keys = Object.keys(obj);
   
   for (const key of keys) {
