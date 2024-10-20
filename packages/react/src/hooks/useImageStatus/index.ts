ce5234e8af589d643a8d1f27d7a545e60f097426
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-10-20 17:40:21 +0900

    ts-doc 추가 (#518)
    * docs: partition docs 수정
    
    * docs: useImageStatus ts-doc 추가

--- packages/react/src/hooks/useImageStatus/index.ts
+++ packages/react/src/hooks/useImageStatus/index.ts
@@ -1,8 +1,37 @@
-import { useCallback, useState } from 'react';
+import { RefCallback, useCallback, useState } from 'react';
 
 type ImageStatus = 'pending' | 'loading' | 'complete' | 'error';
 
-export function useImageStatus() {
+interface UseImageStatusReturnType {
+  ref: RefCallback<HTMLImageElement>;
+  imageStatus: ImageStatus;
+}
+
+/**
+ * @description 이미지 로드 상태를 추적하는 커스텀 훅입니다.
+ *
+ * `pending`, `loading`, `complete`, `error` 네 가지 상태를 반환합니다.
+ *
+ * @returns {UseImageStatusReturnType} - 이미지의 현재 상태와 이미지 요소에 대한 ref 콜백을 반환합니다.
+ * @property {React.RefCallback<HTMLImageElement>} ref - 이미지 요소에 연결할 ref 콜백
+ * @property {ImageStatus} imageStatus - 이미지의 현재 로드 상태 ('pending', 'loading', 'complete', 'error')
+ *
+ * - pending: 아직 이미지 로드가 시작되지 않은 상태.
+ * - loading: 이미지가 로드 중인 상태.
+ * - complete: 이미지가 성공적으로 로드된 상태.
+ * - error: 이미지 로드 중 에러가 발생한 상태.
+ *
+ * @example
+ * const { ref, imageStatus } = useImageStatus();
+ *
+ * return (
+ *   <div>
+ *     <img ref={ref} src="https://example.com/image.jpg" alt="Example" />
+ *     <p>이미지 상태: {imageStatus}</p>
+ *   </div>
+ * );
+ */
+export function useImageStatus(): UseImageStatusReturnType {
   const [imageStatus, setImageStatus] = useState<ImageStatus>('pending');
 
   const ref = useCallback((imgElement: HTMLImageElement) => {
