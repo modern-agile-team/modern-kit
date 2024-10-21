d66f975942067893246bab3a9547ee7459ac24a7
99mini <itman390@gmail.com>
2024-10-21 10:23:06 +0900

    fix: remove overload

--- packages/utils/src/style/formatSizeStyleValue/index.ts
+++ packages/utils/src/style/formatSizeStyleValue/index.ts
@@ -21,16 +21,12 @@ type SuffixUnit =
   | 'lh'
   | 'rlh'
   | '%';
-
-export function formatSizeStyleValue(value: number): string;
-export function formatSizeStyleValue(value: number, suffix: SuffixUnit): string;
-
 /**
  * @description `css` 스타일 값으로 사용할 수 있도록 `<value>[suffix]` 형태로 변환해주는 유틸 합수입니다.
  *
  * `suffix`옵션을 통해서 해당 값에 접미사를 붙여 반환할 수 있습니다. 해당 옵션이 없다면 `default`로 `px`을 접미사로 사용합니다.
  * @param {number} value 점미사를 붙일 값
- * @param {SuffixUnit} suffix `css` 스타일 값의 단위
+ * @param {SuffixUnit} suffix `css` 스타일 값의 단위 값 (default: `px`)
  * @returns {string} `css` 스타일 값으로 사용할 수 있는 값
  * @example
  * ```typescript
@@ -40,7 +36,7 @@ export function formatSizeStyleValue(value: number, suffix: SuffixUnit): string;
  */
 export function formatSizeStyleValue(
   value: number,
-  suffix?: SuffixUnit
+  suffix: SuffixUnit = 'px'
 ): string {
-  return `${value}${suffix ?? 'px'}`;
+  return `${value}${suffix}`;
 }
