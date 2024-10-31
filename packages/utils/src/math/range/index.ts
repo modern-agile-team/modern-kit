3e89cac8ca5d93eac5ac58dbd4fc2c8e2dd025cb
ssi02014 <ssi02014@naver.com>
2024-10-31 16:49:15 +0900

    fix(utils): isInRange, range 로직 개선 및 한글화

--- packages/utils/src/math/range/index.ts
+++ packages/utils/src/math/range/index.ts
@@ -11,20 +11,35 @@ function getRangeValue(start: number, end: number, step: number = 1) {
   return result;
 }
 
+/**
+ * @description 시작 값(start)에서 종료 값(end)까지 주어진 간격(step)에 따라 숫자의 배열을 생성합니다.
+ *
+ * @param {number} start - 생성할 숫자 범위의 시작 값입니다.
+ * @param {number} end - 생성할 숫자 범위의 종료 값입니다.
+ * @param {number} [step] - 숫자의 증가 간격입니다.
+ * @returns {number[]} 시작 값에서 종료 값까지 간격에 따라 생성된 숫자의 배열을 반환합니다.
+ *
+ * @throws {Error} `step`이 0이거나 정수가 아닌 경우 오류가 발생합니다.
+ *
+ * @example
+ * range(5); // [0, 1, 2, 3, 4]
+ *
+ * @example
+ * range(1, 5); // [1, 2, 3, 4]
+ *
+ * @example
+ * range(1, 10, 2); // [1, 3, 5, 7, 9]
+ */
 export function range(end: number): number[];
 export function range(start: number, end: number): number[];
 export function range(start: number, end: number, step?: number): number[];
-export function range(start: number, end?: number, step?: number) {
+export function range(start: number, end?: number, step: number = 1) {
   if (isNil(end)) {
     return getRangeValue(0, start);
   }
 
-  if (isNil(step)) {
-    step = 1;
-  }
-
   if (!Number.isInteger(step) || step === 0) {
-    throw new Error('The step value must be a non-zero integer.');
+    throw new Error('step은 0이 아닌 정수여야 합니다.');
   }
 
   return getRangeValue(start, end, step);
