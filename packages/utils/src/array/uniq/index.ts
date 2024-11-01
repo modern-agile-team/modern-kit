0746b3336f8ad247c27dbb2108f2cc48f09b247e
ssi02014 <ssi02014@naver.com>
2024-11-01 14:06:01 +0900

    fix(utils): uniq 함수 개선 및 한글화

--- packages/utils/src/array/uniq/index.ts
+++ packages/utils/src/array/uniq/index.ts
@@ -1,22 +1,41 @@
+/**
+ * @description 배열에서 중복된 요소를 제거하여 고유한 요소로 이루어진 새 배열을 반환합니다.
+ *
+ * 기본적으로 `원시 값`에 대해서만 중복 요소를 판단합니다.
+ * 2번째 인자인 `iteratee` 함수를 제공하면, 각 요소를 iteratee 반환값에 따라 중복 여부를 판단할 수 있습니다.
+ *
+ * @template T - 배열 요소의 타입
+ * @template U - `iteratee` 함수가 반환하는 값의 타입으로, 중복 판단에 사용됩니다.
+ *
+ * @param {T[] | readonly T[]} arr - 중복을 제거하고자 하는 배열입니다.
+ * @param {(item: T) => U} iteratee - 각 요소를 특정 값으로 변환하여 중복 여부를 판단하는 함수입니다.
+ *
+ * @returns {T[]} 고유한 요소만 포함하는 새 배열을 반환합니다. `iteratee`가 제공되면, 변환된 값 기준으로 중복이 제거된 배열이 반환됩니다.
+ *
+ * @example
+ * uniq([1, 2, 2, 3, 4]); // [1, 2, 3, 4]
+ *
+ * @example
+ * uniq([{ id: 1 }, { id: 2 }, { id: 1 }], item => item.id); // [{ id: 1 }, { id: 2 }]
+ */
 export function uniq<T, U = T>(
   arr: T[] | readonly T[],
   iteratee?: (item: T) => U
-) {
+): T[] {
   if (!iteratee) {
     return Array.from(new Set(arr));
   }
 
-  const set = new Set<U>();
-  const result: T[] = [];
+  const map = new Map<U, T>();
 
-  for (const item of arr) {
-    const appliedIterateeItem = iteratee(item);
+  for (let i = 0; i < arr.length; i++) {
+    const item = arr[i];
+    const key = iteratee(item);
 
-    if (set.has(appliedIterateeItem)) continue;
-
-    set.add(appliedIterateeItem);
-    result.push(item);
+    if (!map.has(key)) {
+      map.set(key, item);
+    }
   }
 
-  return result;
+  return Array.from(map.values());
 }
--- packages/utils/src/array/uniq/uniq.spec.ts
+++ packages/utils/src/array/uniq/uniq.spec.ts
@@ -2,28 +2,21 @@ import { describe, it, expect } from 'vitest';
 import { uniq } from '.';
 
 describe('uniq', () => {
-  it('should return the same array if there are no duplicates', () => {
+  it('중복이 없는 경우 동일한 배열을 반환해야 합니다.', () => {
     const testArr = [1, 2, 3];
     const expectedArray = [1, 2, 3];
 
     expect(uniq(testArr)).toEqual(expectedArray);
   });
 
-  it('should return an array with duplicates removed', () => {
+  it('중복 요소가 제거된 배열을 반환해야 합니다.', () => {
     const testArr = [1, 2, 2, 'a', 2, 'a', 3];
     const expectedArray = [1, 2, 'a', 3];
 
     expect(uniq(testArr)).toEqual(expectedArray);
   });
 
-  it('should be able to determine duplicate elements based on the results of the iteratee(Array)', () => {
-    const testArr = [1, 2, 2.1, 2.2, 2.3, 3];
-    const expectedArray = [1, 2, 3];
-
-    expect(uniq(testArr, (item) => Math.floor(item))).toEqual(expectedArray);
-  });
-
-  it('should be able to determine duplicate elements based on the results of the iteratee(Nested Array)', () => {
+  it('iteratee 결과를 기반으로 중복 요소를 판단 할 수 있어야 합니다.(Nested Array)', () => {
     const testArr = [
       ['a', 'b', 'c'],
       ['a', 'b', 'c'],
@@ -31,13 +24,12 @@ describe('uniq', () => {
     ];
     const expectedArray = [['a', 'b', 'c']];
 
-    expect(uniq(testArr)).toEqual(testArr); // no iteratee
     expect(uniq(testArr, (item) => JSON.stringify(item))).toEqual(
       expectedArray
     );
   });
 
-  it('should be able to determine duplicate elements based on the results of the iteratee(Object)', () => {
+  it('iteratee 결과를 기반으로 중복 요소를 판단 할 수 있어야 합니다.(Object)', () => {
     const testArr = [
       { id: 1, name: 'John' },
       { id: 2, name: 'Jane' },
@@ -51,7 +43,6 @@ describe('uniq', () => {
       { id: 3, name: 'gromit' },
     ];
 
-    expect(uniq(testArr)).toEqual(testArr); // no iteratee
     expect(uniq(testArr, (item) => item.id)).toEqual(expectedArray);
   });
 });
