d956c2b1b28c168c6cf90ac9d7ff966188bd1f0b
ssi02014 <ssi02014@naver.com>
2024-11-06 15:30:18 +0900

    fix(utils): findKey 인터페이스 변경

--- packages/utils/src/object/findKey/findKey.spec.ts
+++ packages/utils/src/object/findKey/findKey.spec.ts
@@ -2,23 +2,25 @@ import { describe, expect, it } from 'vitest';
 import { findKey } from '.';
 
 describe('findKey', () => {
-  it('should return correct key element when a existent key-value is accessed', () => {
+  it('predicate 함수 조건에 부합하는 첫 번째 key를 반환해야 합니다.', () => {
     const obj = {
       bike: { active: true },
       plane: { active: true },
       car: { active: false },
     };
 
-    expect(findKey(obj, (item) => item.active)).toEqual('bike');
+    expect(findKey(obj, ({ value }) => value.active)).toEqual('bike');
   });
 
-  it('should return undefined when a non-existent key-value is accessed', () => {
+  it('predicate 함수 조건에 부합하는 key가 없을 경우 undefined를 반환해야 합니다.', () => {
     const obj = {
       bike: { active: true },
       plane: { active: true },
       car: { active: false },
     };
 
-    expect(findKey(obj, (item) => (item as any).inactive)).toEqual(undefined);
+    expect(
+      findKey(obj, ({ value }) => (value as any).inactive)
+    ).toBeUndefined();
   });
 });
