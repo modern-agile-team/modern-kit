d533f0a5bb7a98ddd44207337fe35e4da423f5e8
ssi02014 <ssi02014@naver.com>
2024-11-07 15:48:33 +0900

    docs: isValidPhoneNumberFormet 한글화

--- packages/utils/src/validator/isValidPhoneNumberFormat/isValidPhoneNumberFormat.spec.ts
+++ packages/utils/src/validator/isValidPhoneNumberFormat/isValidPhoneNumberFormat.spec.ts
@@ -2,20 +2,16 @@ import { describe, it, expect } from 'vitest';
 import { isValidPhoneNumberFormat } from '.';
 
 describe('isValidPhoneNumberFormat', () => {
-  it('should return "true" when the phone number format is valid with hyphens', () => {
+  it('전화 번호 형식이 유효한 경우 true를 반환해야 합니다.', () => {
     expect(isValidPhoneNumberFormat('010-1234-1234')).toBeTruthy();
     expect(isValidPhoneNumberFormat('031-123-1234')).toBeTruthy();
-    expect(isValidPhoneNumberFormat('031-1234-1234')).toBeTruthy();
-    expect(isValidPhoneNumberFormat('070-1234-5678')).toBeTruthy();
     expect(isValidPhoneNumberFormat('02-123-1234')).toBeTruthy();
     expect(isValidPhoneNumberFormat('02-1234-1234')).toBeTruthy();
     expect(isValidPhoneNumberFormat('1234-1234')).toBeTruthy();
-    expect(isValidPhoneNumberFormat('010-1234-1234')).toBeTruthy();
   });
 
-  it('should return "false" when the phone number format is invalid with hyphens', () => {
+  it('전화 번호 형식이 유효하지 않은 경우 false를 반환해야 합니다.', () => {
     expect(isValidPhoneNumberFormat('0-1234-1234')).toBeFalsy();
-    expect(isValidPhoneNumberFormat('0-123-1234')).toBeFalsy();
     expect(isValidPhoneNumberFormat('0101-123-1234')).toBeFalsy();
     expect(isValidPhoneNumberFormat('010-12-1234')).toBeFalsy();
     expect(isValidPhoneNumberFormat('010-12455-1234')).toBeFalsy();
