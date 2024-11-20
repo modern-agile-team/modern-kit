f7d9f93c943e86eb9f38b3cbc8cbc4b0c9b7c480
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-11-20 15:21:13 +0900

    fix(react): AspectRatio 인터페이스 및 내부 로직 개선 (#597)

---
+++ .changeset/polite-apes-rush.md
@@ -0,0 +1,5 @@
+---
+'@modern-kit/react': patch
+---
+
+fix(react): AspectRatio 인터페이스 및 내부 로직 개선 - @ssi0214
--- packages/react/src/components/AspectRatio/index.tsx
+++ packages/react/src/components/AspectRatio/index.tsx
@@ -1,16 +1,10 @@
 import classNames from 'classnames';
 import styles from './AspectRatio.module.css';
 import { Slot } from '../Slot';
-import {
-  CSSProperties,
-  Children,
-  ComponentProps,
-  PropsWithChildren,
-  useMemo,
-} from 'react';
+import { CSSProperties, Children, PropsWithChildren, useMemo } from 'react';
 
-interface AspectRatioProps extends ComponentProps<'div'> {
-  asChild?: boolean;
+interface AspectRatioProps {
+  children: JSX.Element;
   ratio: number;
 }
 
@@ -21,53 +15,33 @@ interface AspectRatioProps extends ComponentProps<'div'> {
  *
  * @template T - 사용할 HTML 요소의 타입을 지정합니다. 기본값은 'div'입니다.
  *
- * @param {PropsWithChildren<AspectRatioProps<T>>} aspectRatioProps - 컴포넌트에 전달되는 속성들입니다.
- * @param {boolean} [aspectRatioProps.asChild=false] - `true`로 설정하면 `Slot` 컴포넌트를 사용해 자식 요소에 aspect-ratio 속성을 적용합니다.
- * @param {number} aspectRatioProps.ratio - 자식 요소의 가로 세로 비율을 지정합니다.
- * @param {React.ReactNode} aspectRatioProps.children - 렌더링 할 자식요소 입니다.
- * @param {Object} [aspectRatioProps.props] - 기타 추가할 나머지 속성들입니다.
+ * @param {PropsWithChildren<AspectRatioProps<T>>} props - 컴포넌트에 전달되는 속성들입니다.
+ * @param {number} props.ratio - 자식 요소의 가로 세로 비율을 지정합니다.
+ * @param {React.ReactNode} props.children - 렌더링 할 자식요소 입니다.
  *
  * @returns {JSX.Element} 주어진 비율에 맞춰 스타일이 적용된 래퍼 요소를 반환합니다.
  *
  * @example
- * // 기본적으로 div를 생성 후 aspect-ratio 속성을 적용합니다.
  * ```tsx
  * <AspectRatio ratio={16 / 9}>
  *  <img src={imgUrl} alt="placeholder" />
  * </AspectRatio>
  * ```
- *
- * @example
- * // asChild 속성을 true로 설정하면 자식 요소에 aspect-ratio 속성을 적용합니다.
- * ```tsx
- * <AspectRatio ratio={16 / 9} asChild>
- *  <section>
- *    <img src={imgUrl} alt="placeholder" />
- *  </section>
- * </AspectRatio>
- * ```
  */
 export const AspectRatio = ({
-  asChild = false,
   ratio,
   children,
-  ...props
-}: PropsWithChildren<AspectRatioProps>): JSX.Element => {
-  const Wrapper = asChild ? Slot : 'div';
+}: AspectRatioProps): JSX.Element => {
   const customStyle: CSSProperties = useMemo(
     () => ({
-      paddingTop: `calc(100% * (1 / ${ratio}))`,
-      ...props.style,
+      aspectRatio: ratio,
     }),
-    [ratio, props.style]
+    [ratio]
   );
 
   return (
-    <Wrapper
-      className={classNames(styles['wrapper'], props.className)}
-      style={customStyle}
-      {...props}>
+    <Slot className={classNames(styles['wrapper'])} style={customStyle}>
       {Children.only(children)}
-    </Wrapper>
+    </Slot>
   );
 };
