37130a8478e851e932f3d07f57585e11bb8641e3
metacode22 <alohajune22@gmail.com>
2024-11-30 15:40:38 +0900

    test(utils): clamp 테스트 코드 수정, benchmark 추가

--- packages/utils/src/math/clamp/clamp.spec.ts
+++ packages/utils/src/math/clamp/clamp.spec.ts
@@ -1,28 +1,18 @@
-import { describe, expect, test } from 'vitest';
+import { describe, expect, it } from 'vitest';
 import { clamp } from '.';
 
 describe('clamp', () => {
-  test('value가 min보다 작을 때 min을 반환한다.', () => {
-    expect(clamp(-1, 0, 10)).toBe(0);
+  it('works with max', () => {
+    expect(clamp(3, 5)).toBe(3);
+    expect(clamp(10, 6)).toBe(6);
   });
 
-  test('value가 max보다 클 때 max를 반환한다.', () => {
-    expect(clamp(11, 0, 10)).toBe(10);
-  });
-
-  test('value가 min과 max 사이에 있을 때 value를 반환한다.', () => {
-    expect(clamp(5, 0, 10)).toBe(5);
-  });
-
-  test('value가 min과 같을 때 value(or min)를 반환한다.', () => {
+  it('works with min and max', () => {
+    expect(clamp(7, 0, 10)).toBe(7);
+    expect(clamp(10, 0, 5)).toBe(5);
+    expect(clamp(-5, 0, 10)).toBe(0);
     expect(clamp(0, 0, 10)).toBe(0);
-  });
-
-  test('value가 max와 같을 때 value(or max)를 반환한다.', () => {
     expect(clamp(10, 0, 10)).toBe(10);
-  });
-
-  test('value가 min과 max가 같을 때 value(or min or max)를 반환한다.', () => {
     expect(clamp(5, 5, 5)).toBe(5);
   });
 });
