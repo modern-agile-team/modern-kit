27b5d17aea761a7edf03ed550b4fcdd6a4024249
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2024-12-01 23:28:02 +0900

    Update packages/utils/src/math/clamp/clamp.spec.ts

--- packages/utils/src/math/clamp/clamp.spec.ts
+++ packages/utils/src/math/clamp/clamp.spec.ts
@@ -2,12 +2,12 @@ import { describe, expect, it } from 'vitest';
 import { clamp } from '.';
 
 describe('clamp', () => {
-  it('works with max', () => {
+  it('max만 있을 경우 최댓값으로 제한해야 됩니다.', () => {
     expect(clamp(3, 5)).toBe(3);
     expect(clamp(10, 6)).toBe(6);
   });
 
-  it('works with min and max', () => {
+  it('min, max가 있을 경우 최솟값과 최댓값 사이로 제한해야 됩니다.', () => {
     expect(clamp(7, 0, 10)).toBe(7);
     expect(clamp(10, 0, 5)).toBe(5);
     expect(clamp(-5, 0, 10)).toBe(0);
