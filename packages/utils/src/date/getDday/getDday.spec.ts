c9a7315d2af7171acc3ff86e856a22309a52f12b
ssi02014 <ssi02014@naver.com>
2024-12-31 19:08:41 +0900

    feat(utils): isDate, getDDay 함수 추가

---
+++ packages/utils/src/date/getDday/getDday.spec.ts
@@ -0,0 +1,91 @@
+import { describe, expect, it, beforeEach, afterEach, vi } from 'vitest';
+import { getDDay } from '.';
+
+beforeEach(() => {
+  // 2025년 1월 1일 00:00:00 로 현재 날짜 고정
+  vi.setSystemTime(new Date('2025-01-01 00:00:00'));
+});
+
+afterEach(() => {
+  vi.useRealTimers();
+});
+
+describe('getDDay', () => {
+  it('목표 날짜가 현재보다 과거인 경우 음수 일수를 반환해야 합니다.', () => {
+    // Date 객체 케이스
+    expect(getDDay(new Date('2024-12-31 00:00:00'))).toEqual({
+      days: -1,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+
+    // 문자열 케이스
+    expect(getDDay('2024-12-25 00:00:00')).toEqual({
+      days: -7,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+  });
+
+  it('목표 날짜가 현재보다 미래인 경우 양수 일수를 반환해야 합니다.', () => {
+    // Date 객체 케이스
+    expect(getDDay(new Date('2025-01-02 00:00:00'))).toEqual({
+      days: 1,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+
+    // 문자열 케이스
+    expect(getDDay('2025-01-05 00:00:00')).toEqual({
+      days: 4,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+  });
+
+  it('시간 차이가 있는 경우 상세 시간을 정확히 계산한다', () => {
+    // Date 객체 케이스
+    expect(getDDay(new Date('2025-01-01 02:30:45'))).toEqual({
+      days: 0,
+      hours: 2,
+      minutes: 30,
+      seconds: 45,
+    });
+
+    // 문자열 케이스
+    expect(getDDay(new Date('2024-12-31 18:15:30'))).toEqual({
+      days: 0,
+      hours: -5,
+      minutes: -44,
+      seconds: -30,
+    });
+  });
+
+  it('목표 날짜가 현재와 같은 경우 모든 값이 0을 반환해야 합니다.', () => {
+    // Date 객체 케이스
+    expect(getDDay(new Date('2025-01-01 00:00:00'))).toEqual({
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+
+    // 문자열 케이스
+    expect(getDDay('2025-01-01 00:00:00')).toEqual({
+      days: 0,
+      hours: 0,
+      minutes: 0,
+      seconds: 0,
+    });
+  });
+
+  it('잘못된 날짜 형식에 대해 에러를 발생시켜야 함', () => {
+    expect(() => getDDay('invalid-date')).toThrow(
+      '유효하지 않은 날짜 형식입니다.'
+    );
+  });
+});
---
+++ packages/utils/src/date/getDday/index.ts
@@ -0,0 +1,63 @@
+import { objectKeys } from '../../object/objectKeys';
+import { isDate } from '../../validator/isDate';
+
+type CalculatedTimeResult = Record<keyof typeof TIME_UNITS, number>;
+
+const TIME_UNITS = {
+  days: 1000 * 60 * 60 * 24,
+  hours: 1000 * 60 * 60,
+  minutes: 1000 * 60,
+  seconds: 1000,
+} as const;
+
+const TIME_UNITS_KEYS = objectKeys(TIME_UNITS);
+
+/**
+ * @description 주어진 날짜와 현재 날짜 사이의 `D-day`를 `일/시/분/초` 단위로 계산합니다.
+ * 목표 날짜가 현재 날짜보다 기간이 남은 경우 `음수`를 반환합니다. 목표 날짜보다 기간이 지난 경우 `양수`를 반환합니다.
+ *
+ * @param {string | number | Date} date - 목표 날짜
+ * @returns {CalculatedTimeResult} - 남은 시간을 `일/시/분/초` 단위에 맞게 계산한 값을 갖는 객체
+ * @throws {Error} 유효하지 않은 날짜 형식이 입력된 경우
+ *
+ * @example
+ * // 현재 날짜가 2025년 1월 1일 00:00:00 일 때
+ * getDDay(new Date('2024-12-25 00:00:00'));
+ * // { days: -7, hours: 0, minutes: 0, seconds: 0 }
+ *
+ * getDDay(new Date('2025-01-01 02:30:45'));
+ * // { days: 0, hours: 2, minutes: 30, seconds: 45 }
+ *
+ * @example
+ * // 문자열 포맷도 허용합니다.
+ * getDDay('2024-12-31 18:15:30');
+ * // { days: 0, hours: -5, minutes: -44, seconds: -30 }
+ */
+export function getDDay(date: string | number | Date): CalculatedTimeResult {
+  const targetDate = isDate(date) ? date : new Date(date);
+
+  // 유효하지 않은 날짜 형식인 경우 에러 발생
+  if (isNaN(targetDate.getTime())) {
+    throw new Error('유효하지 않은 날짜 형식입니다.');
+  }
+
+  const today = new Date();
+  const timeDiff = targetDate.getTime() - today.getTime();
+
+  const isNegative = timeDiff < 0;
+  const absoluteDiff = Math.abs(timeDiff);
+
+  let remainingTime = absoluteDiff;
+
+  const calculateTime = () => {
+    return TIME_UNITS_KEYS.reduce((acc, key) => {
+      const value = Math.floor(remainingTime / TIME_UNITS[key]);
+      remainingTime %= TIME_UNITS[key];
+
+      acc[key] = value && isNegative ? -value : value;
+      return acc;
+    }, {} as CalculatedTimeResult);
+  };
+
+  return calculateTime();
+}
