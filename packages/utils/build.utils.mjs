8f28e1b754a5ffd734be2ff384ba9976f26acd5f
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2025-01-25 15:51:37 +0900

    chore: react/utils build setting update (#709)

--- packages/utils/build.utils.mjs
+++ packages/utils/build.utils.mjs
@@ -1,44 +1,48 @@
+const PATH_PREFIX_MAP = {
+  array: 'array',
+  clipboard: 'clipboard',
+  common: 'common',
+  date: 'date',
+  device: 'device',
+  file: 'file',
+  formatter: 'formatter',
+  math: 'math',
+  object: 'object',
+  regex: 'regex',
+  storage: 'storage',
+  string: 'string',
+  style: 'style',
+  validator: 'validator',
+};
+
 const getEntryFileNames = (name, format, path) => {
   const cleanName = name.replace(`${path}-`, '');
   return `${path}/${cleanName}/index.${format}`;
 };
 
 export const getFormatEntryFileNames = (chunkInfo, format) => {
-  const prefixMap = {
-    array: 'array',
-    clipboard: 'clipboard',
-    common: 'common',
-    date: 'date',
-    device: 'device',
-    file: 'file',
-    formatter: 'formatter',
-    math: 'math',
-    object: 'object',
-    regex: 'regex',
-    storage: 'storage',
-    string: 'string',
-    style: 'style',
-    validator: 'validator',
-  };
+  const { name } = chunkInfo;
 
-  for (const [key, value] of Object.entries(prefixMap)) {
-    if (chunkInfo.name === `${key}-index`) {
+  for (const [key, value] of Object.entries(PATH_PREFIX_MAP)) {
+    // 각 모듈의 index
+    if (name === `${key}-index`) {
       return `${key}/index.${format}`;
     }
 
-    if (chunkInfo.name.startsWith(value)) {
-      return getEntryFileNames(chunkInfo.name, format, key);
+    // 기본 모듈
+    if (name.startsWith(value)) {
+      return getEntryFileNames(name, format, key);
     }
   }
-
-  return `[name].${format}`; // 기본 파일 이름
+  // root index
+  return `[name].${format}`;
 };
 
 export const getSubEntryMap = (keys, path) => {
   return keys.reduce((acc, entry) => {
-    acc[`${path}-${entry}`] = `./src/${path}/${
-      entry === 'index' ? 'index.ts' : `${entry}/index.ts`
-    }`;
+    const entryPath = entry === 'index' ? 'index.ts' : `${entry}/index.ts`;
+
+    acc[`${path}-${entry}`] = `./src/${path}/${entryPath}`;
     return acc;
   }, {});
 };
