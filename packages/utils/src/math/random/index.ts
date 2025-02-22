87f3a1eb2a46be3e99dc34554af89d8edfa361f4
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2025-02-22 16:54:14 +0900

    Update packages/utils/src/math/random/index.ts

--- packages/utils/src/math/random/index.ts
+++ packages/utils/src/math/random/index.ts
@@ -1,40 +1,40 @@
 import { isNil } from '../../validator/isNil';
 
 /**
- * @description 0부터 지정된 최대값 사이의 난수를 생성합니다.
+ * @description 0부터 지정된 최대값 사이의 실수형 난수를 생성합니다.
  *
  * @param {number} maximum - 난수 범위의 최대값입니다.
  * @returns {number} 0 이상 maximum 미만의 무작위 숫자를 반환합니다.
  *
  * @example
- * random(10); // 0 이상 10 미만의 난수를 반환
+ * random(10); // 0 이상 10 미만의 실수형 난수를 반환
  */
 export function random(maximum: number): number;
 
 /**
- * @description 지정된 범위 내에서 난수를 생성합니다.
+ * @description 지정된 범위 내에서 실수형 난수를 생성합니다.
  *
  * @param {number} minimum - 난수 범위의 최소값입니다.
  * @param {number} maximum - 난수 범위의 최대값입니다.
  * @returns {number} minimum 이상 maximum 미만의 난수를 반환합니다.
  *
  * @example
- * random(10, 20); // 10 이상 20 미만의 난수를 반환
+ * random(10, 20); // 10 이상 20 미만의 실수형 난수를 반환
  */
 export function random(minimum: number, maximum: number): number;
 
 /**
- * @description 지정된 범위 내에서 난수를 생성합니다.
+ * @description 지정된 범위 내에서 실수형 난수를 생성합니다.
  *
  * @param {number} minimum - 난수 범위의 최소값입니다.
  * @param {number} [maximum] - 난수 범위의 최대값입니다. 제공되지 않으면 `0`부터 `minimum`까지의 난수를 생성합니다.
  * @returns {number} minimum 이상 maximum 미만의 난수를 반환합니다.
  *
  * @example
- * random(10); // 0 이상 10 미만의 난수를 반환
+ * random(10); // 0 이상 10 미만의 실수형 난수를 반환
  *
  * @example
- * random(5, 15); // 5 이상 15 미만의 난수를 반환
+ * random(5, 15); // 5 이상 15 미만의 실수형 난수를 반환
  */
 export function random(minimum: number, maximum?: number): number {
   if (isNil(maximum)) {
