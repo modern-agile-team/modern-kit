6acfa48acfc52380c4dc15afa9be251c98769280
Minsu <alstntorl@naver.com>
2025-02-22 10:57:08 +0900

    fix(utils): random 함수 피드백 반영 (실수형 난수, 에러 케이스, 함수 오버로딩)

--- packages/utils/src/math/random/index.ts
+++ packages/utils/src/math/random/index.ts
@@ -1,24 +1,51 @@
-import { isUndefined } from 'lodash-es';
+import { isNil } from '../../validator/isNil';
+
+/**
+ * @description 0부터 지정된 최대값 사이의 난수를 생성합니다.
+ *
+ * @param {number} maximum - 난수 범위의 최대값입니다.
+ * @returns {number} 0 이상 maximum 미만의 무작위 숫자를 반환합니다.
+ *
+ * @example
+ * random(10); // 0 이상 10 미만의 난수를 반환
+ */
+export function random(maximum: number): number;
+
+/**
+ * @description 지정된 범위 내에서 난수를 생성합니다.
+ *
+ * @param {number} minimum - 난수 범위의 최소값입니다.
+ * @param {number} maximum - 난수 범위의 최대값입니다.
+ * @returns {number} minimum 이상 maximum 미만의 난수를 반환합니다.
+ *
+ * @example
+ * random(10, 20); // 10 이상 20 미만의 난수를 반환
+ */
+export function random(minimum: number, maximum: number): number;
 
 /**
  * @description 지정된 범위 내에서 난수를 생성합니다.
  *
  * @param {number} minimum - 난수 범위의 최소값입니다.
  * @param {number} [maximum] - 난수 범위의 최대값입니다. 제공되지 않으면 `0`부터 `minimum`까지의 난수를 생성합니다.
- * @returns {number} 지정된 범위 내의 무작위 숫자를 반환합니다.
+ * @returns {number} minimum 이상 maximum 미만의 난수를 반환합니다.
  *
  * @example
- * random(10); // 0 이상 10 이하의 난수를 반환
+ * random(10); // 0 이상 10 미만의 난수를 반환
  *
  * @example
- * random(5, 15); // 5 이상 15 이하의 난수를 반환
- *
+ * random(5, 15); // 5 이상 15 미만의 난수를 반환
  */
 export function random(minimum: number, maximum?: number): number {
-  if (isUndefined(maximum)) {
+  if (isNil(maximum)) {
     maximum = minimum;
     minimum = 0;
   }
-  const rangeSize = maximum - minimum + 1;
-  return Math.floor(Math.random() * rangeSize) + minimum;
+
+  if (minimum > maximum) {
+    throw new Error('maximum은 minimum보다 커야 합니다.');
+  }
+
+  const rangeSize = maximum - minimum;
+  return Math.random() * rangeSize + minimum;
 }
