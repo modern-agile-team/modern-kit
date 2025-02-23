3012ab9562d78ca22fa654e3285a903ec929bdaf
SONGSEUNGSOO <sssong1993@gmail.com>
2025-02-23 13:13:42 +0900

    fix(utils): isBirthDate 함수 로직을 개선

--- packages/utils/src/date/index.ts
+++ packages/utils/src/date/index.ts
@@ -2,5 +2,6 @@ export * from './getAge';
 export * from './getDDay';
 export * from './isAfterDate';
 export * from './isBeforeDate';
+export * from './isBirthDate';
 export * from './isDateInRange';
 export * from './isUnderAge';
--- packages/utils/src/date/isBirthDate/index.ts
+++ packages/utils/src/date/isBirthDate/index.ts
@@ -1,6 +1,96 @@
+import { isNumericString } from '../../validator/isNumericString';
+
+interface ParsedFormatDateReturnType {
+  year: number;
+  month: number;
+  day: number;
+}
+
+const YEAR_SHORT_LENGTH = 6;
+const YEAR_LONG_LENGTH = 8;
+const YEAR_2000 = 2000;
+const YEAR_1900 = 1900;
+
+/**
+ * @description 6자리 생년월일 문자열을 파싱하여 년, 월, 일을 반환합니다.
+ * 주어진 연도가 현재 연도보다 크면 2000년대 연도로 파싱하고, 아니면 1900년대 연도로 파싱합니다.
+ *
+ * ex: 2025년 기준
+ * - 26 ~ 99: 1926 ~ 1999년
+ * - 00 ~ 25: 2000 ~ 2025년
+ *
+ * @param {string} birthDate 6자리 생년월일 문자열
+ * @returns {ParsedFormatDateReturnType} 년, 월, 일
+ */
+const parseShortFormatDate = (
+  birthDate: string
+): ParsedFormatDateReturnType => {
+  const currentYear = new Date().getFullYear(); // 2025
+  const remainingYear = currentYear % 100; // ex 25
+  const birthYear = Number(birthDate.substring(0, 2)); // 03
+
+  return {
+    year:
+      remainingYear > birthYear ? YEAR_2000 + birthYear : YEAR_1900 + birthYear,
+    month: Number(birthDate.substring(2, 4)),
+    day: Number(birthDate.substring(4, 6)),
+  };
+};
+
+/**
+ * @description 8자리 생년월일 문자열을 파싱하여 년, 월, 일을 반환합니다.
+ * @param {string} birthDate 8자리 생년월일 문자열
+ * @returns {ParsedFormatDateReturnType} 년, 월, 일
+ */
+const parseLongFormatDate = (birthDate: string): ParsedFormatDateReturnType => {
+  return {
+    year: Number(birthDate.substring(0, 4)),
+    month: Number(birthDate.substring(4, 6)),
+    day: Number(birthDate.substring(6, 8)),
+  };
+};
+
+/**
+ * @description 구분자가 있는 생년월일 문자열이 올바른 날짜 형식인지 검사합니다.
+ * 'YYYY-MM-DD', 'YY-MM-DD', 'YYYY.MM.DD', 'YY.MM.DD', 'YYYY/MM/DD', 'YY/MM/DD' 형식을 허용합니다.
+ *
+ * @param {string} dateString 생년월일 문자열
+ * @returns {boolean} 올바른 날짜 형식이면 true, 아니면 false
+ */
+const isValidDateFormat = (dateString: string): boolean => {
+  const regex =
+    /^(?:\d{4}|\d{2})[-](?:\d{2})[-](?:\d{2})$|^(?:\d{4}|\d{2})[.](?:\d{2})[.](?:\d{2})$|^(?:\d{4}|\d{2})[\/](?:\d{2})[\/](?:\d{2})$/;
+  return regex.test(dateString);
+};
+
+/**
+ * @description 주어진 날짜가 실제로 존재하는 날짜이며 유효한 범위 내에 있는지 검사합니다.
+ * @param {number} year 년
+ * @param {number} month 월
+ * @param {number} day 일
+ * @returns {boolean} 올바른 날짜이면 true, 아니면 false
+ */
+const isValidDateInRange = (
+  year: number,
+  month: number,
+  day: number
+): boolean => {
+  const date = new Date(year, month - 1, day);
+  const currentYear = new Date().getFullYear();
+  const isValidYearRange = year >= YEAR_1900 && year <= currentYear;
+
+  return (
+    date.getFullYear() === year &&
+    date.getMonth() === month - 1 &&
+    date.getDate() === day &&
+    isValidYearRange
+  );
+};
+
 /**
  * @description 입력된 생년월일 문자열이 6자리 또는 8자리의 올바른 형식인지,
  * 그리고 해당 날짜가 실제로 존재하는 날짜인지(월, 일 범위 및 윤년 고려) 검사합니다.
+ *
  * @param {string} birthDate 생년월일 문자열
  * @returns {boolean} 유효한 날짜면 true, 아니면 false
  *
@@ -23,89 +113,30 @@
  * isBirthDate("199-509-13");  // false, 구분자가 올바른 형식에 위치해야 합니다.
  * isBirthDate("1995-9-13");  // false, 월 또는 일이 2자리여야 합니다.
  */
-
 export function isBirthDate(birthDate: string): boolean {
-  let cleanedBirthDate = '';
+  let birthDateToUse = birthDate;
 
-  if (/[-./]/.test(birthDate)) {
-    const pattern8 = /^(\d{4})([-./])(\d{2})\2(\d{2})$/;
-    const pattern6 = /^(\d{2})([-./])(\d{2})\2(\d{2})$/;
-    const match8 = birthDate.match(pattern8);
-    const match6 = birthDate.match(pattern6);
-
-    if (match8) {
-      cleanedBirthDate = match8[1] + match8[3] + match8[4];
-    } else if (match6) {
-      cleanedBirthDate = match6[1] + match6[3] + match6[4];
-    } else {
-      console.error(
-        '구분자가 올바른 위치에 있지 않습니다. 올바른 형식은 YYYY-MM-DD, YY-MM-DD, YYYY.MM.DD, YY.MM.DD, YYYY/MM/DD, YY/MM/DD 입니다.'
-      );
-      return false;
-    }
-  } else {
-    if (!/^\d{6}$|^\d{8}$/.test(birthDate)) {
-      console.error(
-        '유효한 생년월일이 아닙니다. 6자리(YYMMDD) 또는 8자리(YYYYMMDD)만 허용됩니다.'
-      );
-      return false;
-    }
-    cleanedBirthDate = birthDate;
+  if (isValidDateFormat(birthDateToUse)) {
+    birthDateToUse = birthDateToUse.replace(/[-/.]/g, '');
   }
 
-  let year: number, month: number, day: number;
-
-  if (cleanedBirthDate.length === 6) {
-    const yy = parseInt(cleanedBirthDate.substring(0, 2), 10);
-    const candidateYear = 2000 + yy;
-    const currentYear = new Date().getFullYear();
-    year = candidateYear > currentYear ? 1900 + yy : candidateYear;
-    month = parseInt(cleanedBirthDate.substring(2, 4), 10);
-    day = parseInt(cleanedBirthDate.substring(4, 6), 10);
-  } else {
-    year = parseInt(cleanedBirthDate.substring(0, 4), 10);
-    month = parseInt(cleanedBirthDate.substring(4, 6), 10);
-    day = parseInt(cleanedBirthDate.substring(6, 8), 10);
-  }
-
-  if (month < 1 || month > 12 || day < 1 || day > 31) {
-    console.error('정확한 날짜를 입력해주세요.');
+  if (!isNumericString(birthDateToUse)) {
     return false;
   }
 
-  const daysInMonth: Record<number, number> = {
-    1: 31,
-    2: isLeapYear(year) ? 29 : 28,
-    3: 31,
-    4: 30,
-    5: 31,
-    6: 30,
-    7: 31,
-    8: 31,
-    9: 30,
-    10: 31,
-    11: 30,
-    12: 31,
-  };
-
-  if (day > daysInMonth[month]) {
-    console.error('정확한 날짜를 입력해주세요.');
+  if (
+    birthDateToUse.length !== YEAR_SHORT_LENGTH &&
+    birthDateToUse.length !== YEAR_LONG_LENGTH
+  ) {
     return false;
   }
 
-  return true;
-}
+  const parsedBirthDate =
+    birthDateToUse.length === YEAR_SHORT_LENGTH
+      ? parseShortFormatDate(birthDateToUse)
+      : parseLongFormatDate(birthDateToUse);
 
-/**
- * @description 주어진 연도가 윤년인지 판별하는 함수입니다.
- * 윤년 조건 : 연도가 4의 배수이면서 10의 배수가 아니거나, 400의 배수인 경우
- * @param {number} year 검사할 연도
- * @returns {boolean} 윤년이면 true , 아니면 false
- *
- * @example
- * isLeapYear(2020); // true (2020년은 윤년)
- * isLeapYear(2019); // false (2019년은 윤년이 아님)
- */
-function isLeapYear(year: number): boolean {
-  return (year % 4 === 0 && year % 100 !== 0) || year % 400 === 0;
+  const { year, month, day } = parsedBirthDate;
+
+  return isValidDateInRange(year, month, day);
 }
