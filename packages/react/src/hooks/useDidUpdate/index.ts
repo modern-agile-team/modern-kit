4d9e1a32537a29f3e53cab4fcda67c221b0b73f0
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2025-02-25 16:05:39 +0900

    Update packages/react/src/hooks/useDidUpdate/index.ts

--- packages/react/src/hooks/useDidUpdate/index.ts
+++ packages/react/src/hooks/useDidUpdate/index.ts
@@ -1,19 +1,18 @@
 import { DependencyList, useEffect, useRef } from 'react';
 
 /**
- * @description 최초 마운트시에는 실행되지않고 deps값이 업데이트 되었을때 effect를 실행시키는 커스텀 훅입니다.
+ * @description 최초 마운트 시에는 실행되지 않고, 의존성 배열(`deps`)의 값이 업데이트되었을 때만 effect를 실행시키는 커스텀 훅입니다.
  *
- * @param effect 실행할 함수입니다.
- * @param deps effect를 다시 실행 할 의존성 배열입니다.
+ * @param {() => void} effect - 실행할 함수입니다.
+ * @param {DependencyList} deps - effect를 다시 실행 할 의존성 배열입니다.
  *
- * @returns void
+ * @returns {void} void
  *
  * @example
  * useDidUpdate(() => {
  *  console.log('deps가 변경되었습니다.');
- * }, [deps]);
+ * }, deps);
  */
-
 export function useDidUpdate(effect: () => void, deps: DependencyList): void {
   const isMounted = useRef(false);
 
