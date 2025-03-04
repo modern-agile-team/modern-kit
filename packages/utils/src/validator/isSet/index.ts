c854dd888409e444e37b8c517d00778b33ccdb85
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2025-03-04 12:10:41 +0900

    Update packages/utils/src/validator/isSet/index.ts

--- packages/utils/src/validator/isSet/index.ts
+++ packages/utils/src/validator/isSet/index.ts
@@ -1,7 +1,7 @@
 /**
- * @description 주어진 값이 `Set` 타입인지 확인합니다.
+ * @description 주어진 인자가 `Set` 객체인지 검사하는 함수입니다.
  *
- * 타입 체크 및 변수의 타입을 `Set<T>`로 좁히는 데 유용합니다.
+ * 타입 체크 및 인자의 타입을 `Set`으로 좁히는 데 유용합니다.
  *
  * @param {unknown} value - 확인할 값입니다.
  * @returns {value is Set<T>} 값이 `Set` 객체일 경우 `true`, 그렇지 않으면 `false`를 반환합니다.
