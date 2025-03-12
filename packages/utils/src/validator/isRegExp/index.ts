53aaab88d3200fbd70a251875fc139d83841cc41
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2025-03-13 03:20:55 +0900

    Apply suggestions from code review

--- packages/utils/src/validator/isRegExp/index.ts
+++ packages/utils/src/validator/isRegExp/index.ts
@@ -1,7 +1,5 @@
 /**
- * @description 주어진 인자가 `RegExp` 인지 검사하는 함수입니다.
- *
- * 타입 체크 및 인자의 타입을 `RegExp`으로 좁히는 데 유용합니다.
+ * @description 주어진 인자가 `RegExp` 인지 검사하고, 맞다면 인자의 타입을 `RegExp`로 좁혀주는 함수입니다.
  *
  * @param {unknown} value - 확인할 값입니다.
  * @returns {value is RegExp} 값이 `RegExp` 인스턴스일 경우 `true`, 그렇지 않으면 `false`를 반환합니다.
