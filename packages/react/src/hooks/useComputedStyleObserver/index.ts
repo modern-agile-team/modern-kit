cded639827fe4cfce22b62ea0319e308bc407e92
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2025-03-16 17:52:40 +0900

    feat(react): useComputedStyleObserver 신규 훅 추가 (#792)

---
+++ packages/react/src/hooks/useComputedStyleObserver/index.ts
@@ -0,0 +1,82 @@
+import React, { useCallback, useEffect, useRef, useState } from 'react';
+
+interface UseComputedStyleObserverReturnType<T extends HTMLElement> {
+  ref: React.RefObject<T>;
+  value: string;
+  setValue: (value: string) => void;
+}
+
+/**
+ * @description 관찰할 타겟 요소의 특정 computed style 값을 실시간으로 관찰하고 관리하는 React 훅
+ *
+ * MutationObserver를 사용하여 요소의 스타일 속성이 변경될 때 콜백을 호출합니다.
+ * @see https://developer.mozilla.org/en-US/docs/Web/API/MutationObserver/observe
+ *
+ * @template T - 관찰할 HTML 요소 타입, 기본적으로 `HTMLElement`를 상속합니다.
+ * @param {string} key - 관찰할 스타일 속성 키
+ * @returns {UseComputedStyleObserverReturnType<T>} 다음을 포함하는 객체:
+ * - `ref`: 관찰할 타겟 요소에 전달할 ref
+ * - `value`: 현재 계산된 스타일 속성 값
+ * - `setValue`: 관찰중인 스타일 속성 Key에 해당하는 속성 값을 업데이트하는 함수
+ *
+ * @example
+ * ```tsx
+ * const Example = () => {
+ *   const { ref, value, setValue } = useComputedStyleObserver<HTMLDivElement>('color');
+ *
+ *   return (
+ *     <div ref={ref} style={{ color: "black" }}>
+ *       현재 글자 색상: {value}
+ *       <button onClick={() => setValue('red')}>빨간색으로 변경</button>
+ *     </div>
+ *   );
+ * };
+ * ```
+ */
+export function useComputedStyleObserver<T extends HTMLElement>(
+  key: string
+): UseComputedStyleObserverReturnType<T> {
+  const ref = useRef<T>(null);
+  const observerRef = useRef<MutationObserver | null>(null);
+  const [value, setValue] = useState('');
+
+  const setStyleValue = useCallback(
+    (value: string) => {
+      if (!ref.current) return;
+      const targetElement = ref.current;
+
+      targetElement.style.setProperty(key, value);
+      setValue(value);
+    },
+    [key]
+  );
+
+  const updateStyleValue = useCallback(() => {
+    if (!ref.current) return;
+    const targetElement = ref.current;
+
+    const computedStyle = window.getComputedStyle(targetElement);
+    const value = computedStyle.getPropertyValue(key).trim();
+
+    setValue(value);
+  }, [key]);
+
+  useEffect(() => {
+    if (!ref.current) return;
+
+    const element = ref.current;
+
+    updateStyleValue();
+    observerRef.current = new MutationObserver(updateStyleValue);
+
+    observerRef.current.observe(element, {
+      attributeFilter: ['style', 'class'], // style, class 속성 변경을 관찰
+    });
+
+    return () => {
+      observerRef.current?.disconnect();
+    };
+  }, [updateStyleValue]);
+
+  return { ref, value, setValue: setStyleValue };
+}
---
+++ packages/react/src/hooks/useComputedStyleObserver/useComputedStyleObserver.spec.tsx
@@ -0,0 +1,45 @@
+import { describe, it, expect } from 'vitest';
+import { screen, waitFor } from '@testing-library/react';
+import { useComputedStyleObserver } from '.';
+import { renderSetup } from '../../_internal/test/renderSetup';
+
+const TestComponent = () => {
+  const { ref, value, setValue } =
+    useComputedStyleObserver<HTMLDivElement>('width');
+
+  return (
+    <div ref={ref} style={{ width: '100px' }}>
+      <p role="paragraph">{`${value}`}</p>
+      <button onClick={() => setValue('200px')}>setValue 변경</button>
+      <button onClick={() => ref.current?.style.setProperty('width', '200px')}>
+        setProperty 변경
+      </button>
+    </div>
+  );
+};
+
+describe('useComputedStyleObserver', () => {
+  it('value는 초기 상태가 100px이어야 합니다.', () => {
+    renderSetup(<TestComponent />);
+
+    expect(screen.getByRole('paragraph')).toHaveTextContent('100px');
+  });
+
+  it('setValue 함수를 호출하면 이를 실시간으로 반영해야 합니다.', async () => {
+    const { user } = renderSetup(<TestComponent />);
+
+    await user.click(screen.getByRole('button', { name: 'setValue 변경' }));
+
+    expect(screen.getByRole('paragraph')).toHaveTextContent('200px');
+  });
+
+  it('setProperty로 직접 스타일을 변경하더라도 이를 실시간으로 반영해야 합니다.', async () => {
+    const { user } = renderSetup(<TestComponent />);
+
+    await user.click(screen.getByRole('button', { name: 'setProperty 변경' }));
+
+    await waitFor(() => {
+      expect(screen.getByRole('paragraph')).toHaveTextContent('200px');
+    });
+  });
+});
