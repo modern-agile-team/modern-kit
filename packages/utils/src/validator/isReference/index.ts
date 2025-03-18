5a3b297c0440a12289991b49d1af5646a53bcbad
ssi02014 <ssi02014@naver.com>
2025-03-18 17:07:37 +0900

    docs: useEventListener ts-doc 세분화

--- packages/utils/src/validator/isReference/index.ts
+++ packages/utils/src/validator/isReference/index.ts
@@ -1,12 +1,24 @@
 import { isPrimitive } from '../isPrimitive';
 
+type Reference =
+  | Record<PropertyKey, any>
+  | any[]
+  | ((...args: any[]) => any)
+  | Set<any>
+  | Map<any, any>
+  | WeakMap<object, any>
+  | WeakSet<object>
+  | Date
+  | RegExp
+  | Error;
+
 /**
  * @description 주어진 값이 참조 타입(Reference)인지 확인합니다.
  *
  * 이 함수는 값이 원시 타입이 아닌 경우, 즉 객체나 함수와 같은 참조 타입인지 여부를 검사합니다.
  *
  * @param {unknown} value - 참조 타입인지 확인할 값.
- * @returns {value is Record<PropertyKey, any>} - 값이 참조 타입이면 `true`, 그렇지 않으면 `false`를 반환합니다.
+ * @returns {value is Reference} - 값이 참조 타입이면 `true`, 그렇지 않으면 `false`를 반환합니다.
  *
  * @example
  * isReference({}); // true
@@ -17,6 +29,6 @@ import { isPrimitive } from '../isPrimitive';
  * isReference(42); // false
  * isReference("hello"); // false
  */
-export function isReference(value: unknown): value is Record<PropertyKey, any> {
+export function isReference(value: unknown): value is Reference {
   return !isPrimitive(value);
 }
