81d4dcda058042cfe2152a6ac072becd2340f94a
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2025-03-30 15:10:25 +0900

    test: 수정 (#812)

--- packages/react/src/hooks/useMediaQuery/useMediaQuery.spec.tsx
+++ packages/react/src/hooks/useMediaQuery/useMediaQuery.spec.tsx
@@ -2,7 +2,7 @@ import { describe, it, expect, beforeEach, vi, MockInstance } from 'vitest';
 
 import { useMediaQuery } from '.';
 import { renderHook } from '@testing-library/react';
-import * as ModernKitUtils from '@modern-kit/utils';
+import { renderToString } from 'react-dom/server';
 
 let windowMatchMediaSpy: MockInstance;
 
@@ -18,6 +18,16 @@ beforeEach(() => {
   });
 });
 
+const TestComponent = ({
+  initialValue = false,
+}: {
+  initialValue?: boolean;
+}) => {
+  const mediaQuery = useMediaQuery('(min-width: 600px)', initialValue);
+
+  return <p role="paragraph">{`${mediaQuery}`}</p>;
+};
+
 describe('useMediaQuery', () => {
   it('쿼리가 일치할 때 true를 반환해야 합니다', () => {
     const { result } = renderHook(() => useMediaQuery('(min-width: 600px)'));
@@ -31,21 +41,9 @@ describe('useMediaQuery', () => {
     expect(result.current).toBe(false);
   });
 
-  it('클라이언트 환경이 아닐 때 false를 반환해야 합니다', () => {
-    vi.spyOn(ModernKitUtils, 'isClient').mockReturnValue(false);
-
-    const { result } = renderHook(() => useMediaQuery('(min-width: 600px)'));
-
-    expect(result.current).toBe(false);
-  });
-
   it('defaultValue가 제공되면, 클라이언트 환경이 아닐 때 defaultValue를 반환해야 합니다', () => {
-    vi.spyOn(ModernKitUtils, 'isClient').mockReturnValue(false);
+    const html = renderToString(<TestComponent initialValue={true} />);
 
-    const { result } = renderHook(() =>
-      useMediaQuery('(min-width: 600px)', true)
-    );
-
-    expect(result.current).toBe(true);
+    expect(html).toContain('true');
   });
 });
