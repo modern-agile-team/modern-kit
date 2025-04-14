edc94028f6fdea70762c36c4f69730d3ff93844a
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2025-04-14 18:55:14 +0900

    fix: useDebounceInputValue 개선 (#839)

--- packages/react/src/hooks/useDebouncedInputValue/index.ts
+++ packages/react/src/hooks/useDebouncedInputValue/index.ts
@@ -1,5 +1,5 @@
 import { ChangeEvent, useCallback, useState } from 'react';
-import { DebounceParameters } from '../../hooks';
+import { type DebounceParameters } from '../../hooks/useDebounce';
 import { useDebouncedState } from '../../hooks/useDebouncedState';
 
 interface UseDebouncedInputValueReturnType {
@@ -14,6 +14,7 @@ interface UseDebouncedInputValueReturnType {
  *
  * 현재 입력 값과 디바운싱된 값을 제공하며, 값을 업데이트하고 리셋할 수 있는 함수도 함께 제공합니다.
  *
+ * @param {string} initialValue - 초기 입력 값입니다.
  * @param {DebounceParameters[1]} [wait] - 입력 값에 디바운싱을 적용할 때의 지연 시간(밀리초)입니다.
  * @param {DebounceParameters[2]} [options={}] - 디바운스 동작에 대한 선택적 구성 옵션입니다.
  *
@@ -29,12 +30,13 @@ interface UseDebouncedInputValueReturnType {
  * debouncedValue; // 300ms 이후에 변경
  */
 export function useDebouncedInputValue(
+  initialValue: string,
   wait: DebounceParameters[1],
   options: DebounceParameters[2] = {}
 ): UseDebouncedInputValueReturnType {
-  const [value, setValue] = useState('');
+  const [value, setValue] = useState(initialValue);
   const [debouncedValue, setDebouncedValue] = useDebouncedState(
-    '',
+    initialValue,
     wait,
     options
   );
@@ -49,8 +51,8 @@ export function useDebouncedInputValue(
   );
 
   const onReset = useCallback(() => {
-    setValue('');
-    setDebouncedValue('');
+    setValue(initialValue);
+    setDebouncedValue(initialValue);
   }, [setDebouncedValue]);
 
   return { value, debouncedValue, onChange, onReset };
