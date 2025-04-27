5bf7247a082dcea2d8772cc793ed81e2b42bfcc1
ssi02014 <ssi02014@naver.com>
2025-04-27 18:04:17 +0900

    fix(react): snapshot 기능 추가

--- packages/react/src/hooks/useStateWithHistory/useStateWithHistory.spec.ts
+++ packages/react/src/hooks/useStateWithHistory/useStateWithHistory.spec.ts
@@ -13,10 +13,8 @@ describe('useStateWithHistory', () => {
 
       // history: [0, 1]
       await waitFor(() => push(1));
-
       // history: [0, 1, 2]
       await waitFor(() => push(2));
-
       // history: [0, 1, 2, 3]
       await waitFor(() => push(3));
 
@@ -39,10 +37,8 @@ describe('useStateWithHistory', () => {
 
       // history: [0, 1]
       await waitFor(() => push(1));
-
       // history: [0, 1, 2]
       await waitFor(() => push(2));
-
       // history: [1, 2, 3]
       await waitFor(() => push(3));
 
@@ -55,6 +51,50 @@ describe('useStateWithHistory', () => {
       await waitFor(() => goToIndex(2));
 
       expect(result.current.state).toBe(3);
+    });
+
+    it('스냅샷을 저장할 수 있어야 합니다', async () => {
+      const { result } = renderHook(() => useStateWithHistory(0));
+
+      const { push, undo, redo, saveSnapshot, restoreSnapshot } =
+        result.current;
+
+      // history: [0, 1]
+      await waitFor(() => push(1));
+      // history: [0, 1, 2]
+      await waitFor(() => push(2));
+
+      // history: [0, 1, 2]
+      expect(result.current.state).toBe(2);
+
+      saveSnapshot(); // snapshot: 2
+
+      // history: [0, 1, 2, 3]
+      await waitFor(() => push(3));
+
+      // history: [0, 1, 2, 3, 2]
+      await waitFor(() => restoreSnapshot());
+
+      expect(result.current.state).toBe(2);
+
+      await waitFor(() => undo());
+      expect(result.current.state).toBe(3);
+
+      await waitFor(() => redo());
+      expect(result.current.state).toBe(2);
+    });
+
+    it('초기 상태 및 상태 변경 함수를 함수로 전달할 수 있어야 합니다', async () => {
+      const { result } = renderHook(() => useStateWithHistory(() => 0));
+
+      const { push } = result.current;
+
+      expect(result.current.state).toBe(0);
+
+      // history: [0, 1]
+      await waitFor(() => push((prev) => prev + 1));
+
+      expect(result.current.state).toBe(1);
     });
   });
 
@@ -102,6 +142,14 @@ describe('useStateWithHistory', () => {
 
       // history: [0]
       expect(() => result.current.goToIndex(1)).toThrow();
+    });
+
+    it('스냅샷이 없을 때 복원 시 에러가 발생해야 합니다', () => {
+      const { result } = renderHook(() => useStateWithHistory(0));
+
+      const { restoreSnapshot } = result.current;
+
+      expect(() => restoreSnapshot()).toThrow('저장된 스냅샷이 없습니다');
     });
   });
 });
