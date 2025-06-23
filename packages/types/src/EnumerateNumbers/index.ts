96a7310950f8d130b87ed2925c818ee2986c1aa4
zzzRYT <jinjinstar3@gmail.com>
2025-06-23 19:49:20 +0900

    feat(types): 주석 추가 및 index.ts변경

--- packages/types/src/EnumerateNumbers/index.ts
+++ packages/types/src/EnumerateNumbers/index.ts
@@ -1,3 +1,13 @@
+/**
+ * @description 0부터 N-1까지의 숫자 유니언 타입을 생성합니다. N이 number타입이 아닐경우 never를 반환합니다.
+ *
+ * @template N - 생성할 숫자 범위의 상한값 (5면 0, 1, 2, 3, 4를 생성)
+ * @template Acc - 재귀적으로 누적되는 배열 타입, 기본값은 빈 배열입니다.
+ *
+ * @example
+ * type zeroToFive = EnumerateNumbers<6>; // 0 | 1 | 2 | 3 | 4 | 5
+ * type otherTypeEnumerate = EnumerateNumbers<'6'>; // never
+ */
 export type EnumerateNumbers<N, Acc extends number[] = []> = N extends number
   ? Acc['length'] extends N
     ? Acc[number]
