c116698f522f14c1eef310a9fd3529e5ed48675d
zzzRYT <jinjinstar3@gmail.com>
2025-06-24 18:53:18 +0900

    test(types): 성공 실패 로직 분리

--- packages/types/src/EnumerateNumbers/EnumerateNumbers.spec.ts
+++ packages/types/src/EnumerateNumbers/EnumerateNumbers.spec.ts
@@ -2,9 +2,13 @@ import { describe, it, expectTypeOf } from 'vitest';
 import { EnumerateNumbers } from '.';
 
 describe('EnumerateNumbers', () => {
-  it('number타입이라면 0 ~ N-1까지 유니언 타입을 반환하고, number타입이 아니라면 never타입을 반환합니다.', () => {
+  it('number 타입이라면 0 ~ N-1까지의 숫자 유니언 타입을 생성합니다.', () => {
     const validEnumerate: EnumerateNumbers<6> = 4;
 
+    expectTypeOf(validEnumerate).toEqualTypeOf<4>();
+  });
+
+  it('number타입이 아니라면 never타입을 반환합니다.', () => {
     const stringEnumerate: EnumerateNumbers<'6'> =
       '6' as unknown as EnumerateNumbers<'6'>;
     const booleanEnumerate: EnumerateNumbers<true> =
@@ -16,7 +20,6 @@ describe('EnumerateNumbers', () => {
       1, 2, 3,
     ] as unknown as EnumerateNumbers<number[]>;
 
-    expectTypeOf(validEnumerate).toEqualTypeOf<4>();
     expectTypeOf(stringEnumerate).toEqualTypeOf<never>();
     expectTypeOf(booleanEnumerate).toEqualTypeOf<never>();
     expectTypeOf(objectEnumerate).toEqualTypeOf<never>();
