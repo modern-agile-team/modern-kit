8794a2b874868df2464781188dbe15fa28a058b9
zzzRYT <jinjinstar3@gmail.com>
2025-06-24 17:53:34 +0900

    refactor(types): EnumerateNumbers 전달 값 하나로 조정

--- packages/types/src/EnumerateNumbers/EnumerateNumbers.spec.ts
+++ packages/types/src/EnumerateNumbers/EnumerateNumbers.spec.ts
@@ -2,18 +2,24 @@ import { describe, it, expectTypeOf } from 'vitest';
 import { EnumerateNumbers } from '.';
 
 describe('EnumerateNumbers', () => {
-  it('0 ~ N-1 까지 수라면 해당 정상적으로 타입을 반환하고, 그 외 숫자라면 never 타입을 반환합니다.', () => {
-    const validEnumerate: EnumerateNumbers<5> = 3;
-    const inValidEnumerate = 6 as unknown as EnumerateNumbers<5>;
+  it('number타입이라면 0 ~ N-1까지 유니언 타입을 반환하고, number타입이 아니라면 never타입을 반환합니다.', () => {
+    const validEnumerate: EnumerateNumbers<6> = 4;
 
-    expectTypeOf(validEnumerate).toEqualTypeOf<3>();
-    expectTypeOf(inValidEnumerate).not.toMatchTypeOf<6>();
-  });
+    const stringEnumerate: EnumerateNumbers<'6'> =
+      '6' as unknown as EnumerateNumbers<'6'>;
+    const booleanEnumerate: EnumerateNumbers<true> =
+      true as unknown as EnumerateNumbers<true>;
+    const objectEnumerate: EnumerateNumbers<{ value: 6 }> = {
+      value: 6,
+    } as unknown as EnumerateNumbers<{ value: 6 }>;
+    const arrayEnumerate: EnumerateNumbers<number[]> = [
+      1, 2, 3,
+    ] as unknown as EnumerateNumbers<number[]>;
 
-  it('Acc 배열을 사용하여 시작 인덱스를 조정할 수 있습니다.', () => {
-    type TwoToFive = EnumerateNumbers<6, [0, 0]>;
-    const startTwo = 3 as unknown as TwoToFive;
-
-    expectTypeOf(startTwo).toEqualTypeOf<2 | 3 | 4 | 5>();
+    expectTypeOf(validEnumerate).toEqualTypeOf<4>();
+    expectTypeOf(stringEnumerate).toEqualTypeOf<never>();
+    expectTypeOf(booleanEnumerate).toEqualTypeOf<never>();
+    expectTypeOf(objectEnumerate).toEqualTypeOf<never>();
+    expectTypeOf(arrayEnumerate).toEqualTypeOf<never>();
   });
 });
