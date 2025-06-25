9aa2ec0d7c59181ac195d0fb357c1f58833ec03c
zzzRYT <jinjinstar3@gmail.com>
2025-06-25 17:14:21 +0900

    refactor(types): test제네릭 타입으로 명시하도록 변경

--- packages/types/src/Range/Range.spec.ts
+++ packages/types/src/Range/Range.spec.ts
@@ -2,26 +2,14 @@ import { describe, it, expectTypeOf } from 'vitest';
 import { Range } from '.';
 
 describe('Range', () => {
-  it('범위 내 숫자라면 정상 출력하고, 범위 밖 숫자라면 타입에러를 발생시킵니다.', () => {
-    const inRangeInteger: Range<1, 5> = 4;
-    const outOfRangeInteger = 6 as unknown as Range<1, 5>;
-
-    expectTypeOf(inRangeInteger).toEqualTypeOf<4>();
-    expectTypeOf(outOfRangeInteger).not.toMatchTypeOf<6>();
+  it('주어진 범위 내의 숫자들의 유니온 타입을 반환해야 합니다.', () => {
+    expectTypeOf<Range<1, 5>>().toEqualTypeOf<1 | 2 | 3 | 4>();
   });
 
-  it('<number, number>타입이 아니라면 never를 반환합니다.', () => {
-    const otherTypeRange: Range<1, '5'> = null as unknown as Range<1, '5'>;
-    const otherTypeRange2: Range<'1', 5> = null as unknown as Range<'1', 5>;
-    const otherTypeRange3: Range<'1', '5'> = null as unknown as Range<'1', '5'>;
-    const otherTypeRange4: Range<false, true> = null as unknown as Range<
-      false,
-      true
-    >;
-
-    expectTypeOf(otherTypeRange).toEqualTypeOf<never>();
-    expectTypeOf(otherTypeRange2).toEqualTypeOf<never>();
-    expectTypeOf(otherTypeRange3).toEqualTypeOf<never>();
-    expectTypeOf(otherTypeRange4).toEqualTypeOf<never>();
+  it('매개변수가 모두 숫자 타입이 아닐 경우 never를 반환해야 합니다', () => {
+    expectTypeOf<Range<1, '5'>>().toEqualTypeOf<never>();
+    expectTypeOf<Range<'1', 5>>().toEqualTypeOf<never>();
+    expectTypeOf<Range<'1', '5'>>().toEqualTypeOf<never>();
+    expectTypeOf<Range<false, true>>().toEqualTypeOf<never>();
   });
 });
