694e5c5b5b495f59998a4c3bd2853ca73ee6140d
zzzRYT <jinjinstar3@gmail.com>
2025-06-25 17:20:04 +0900

    refactor(types): EnumerateNumbers test 제네릭 타입 명시로 변경

--- packages/types/src/EnumerateNumbers/EnumerateNumbers.spec.ts
+++ packages/types/src/EnumerateNumbers/EnumerateNumbers.spec.ts
@@ -2,27 +2,14 @@ import { describe, it, expectTypeOf } from 'vitest';
 import { EnumerateNumbers } from '.';
 
 describe('EnumerateNumbers', () => {
-  it('number 타입이라면 0 ~ N-1까지의 숫자 유니언 타입을 생성합니다.', () => {
-    const validEnumerate: EnumerateNumbers<6> = 4;
-
-    expectTypeOf(validEnumerate).toEqualTypeOf<4>();
+  it('제네릭 타입이 number일 때 0부터 N-1까지의 숫자 유니언 타입을 반환해야 합니다', () => {
+    expectTypeOf<EnumerateNumbers<6>>().toEqualTypeOf<0 | 1 | 2 | 3 | 4 | 5>();
   });
 
-  it('number타입이 아니라면 never타입을 반환합니다.', () => {
-    const stringEnumerate: EnumerateNumbers<'6'> =
-      '6' as unknown as EnumerateNumbers<'6'>;
-    const booleanEnumerate: EnumerateNumbers<true> =
-      true as unknown as EnumerateNumbers<true>;
-    const objectEnumerate: EnumerateNumbers<{ value: 6 }> = {
-      value: 6,
-    } as unknown as EnumerateNumbers<{ value: 6 }>;
-    const arrayEnumerate: EnumerateNumbers<number[]> = [
-      1, 2, 3,
-    ] as unknown as EnumerateNumbers<number[]>;
-
-    expectTypeOf(stringEnumerate).toEqualTypeOf<never>();
-    expectTypeOf(booleanEnumerate).toEqualTypeOf<never>();
-    expectTypeOf(objectEnumerate).toEqualTypeOf<never>();
-    expectTypeOf(arrayEnumerate).toEqualTypeOf<never>();
+  it('제네릭 타입이 number가 아닐 때 never 타입을 반환해야 합니다', () => {
+    expectTypeOf<EnumerateNumbers<'6'>>().toEqualTypeOf<never>();
+    expectTypeOf<EnumerateNumbers<true>>().toEqualTypeOf<never>();
+    expectTypeOf<EnumerateNumbers<{ value: 6 }>>().toEqualTypeOf<never>();
+    expectTypeOf<EnumerateNumbers<number[]>>().toEqualTypeOf<never>();
   });
 });
