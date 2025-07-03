3b82d77e3f0bf8b5975d3c50c34df2c47644f35a
ssi02014 <ssi02014@naver.com>
2025-07-03 17:03:07 +0900

    test: throttle 테스트 코드 개선

--- packages/utils/src/common/throttle/throttle.spec.ts
+++ packages/utils/src/common/throttle/throttle.spec.ts
@@ -10,60 +10,28 @@ afterEach(() => {
   vi.useRealTimers();
 });
 
+const DELAY_TIME = 50;
+
 describe('throttle', () => {
   it('함수 호출을 스로틀링해야 한다', () => {
     const func = vi.fn();
-    const wait = 50;
-    const throttledFunc = throttle(func, wait);
 
-    throttledFunc();
-    throttledFunc();
-    throttledFunc();
-
-    expect(func).toHaveBeenCalledTimes(1);
-
-    vi.advanceTimersByTime(wait);
-
-    expect(func).toHaveBeenCalledTimes(2);
-  });
-
-  it('대기 시간 내에 호출되지 않으면 함수가 즉시 실행되어야 합니다.', async () => {
-    const func = vi.fn();
-    const wait = 50;
-    const throttledFunc = throttle(func, wait);
+    const throttledFunc = throttle(func, DELAY_TIME);
 
     throttledFunc(); // 첫 호출은 즉시 실행
     expect(func).toHaveBeenCalledTimes(1);
 
-    vi.advanceTimersByTime(wait / 2);
-    expect(func).toHaveBeenCalledTimes(1);
-
     throttledFunc(); // 대기 시간 내에 호출되기 때문에 무시
-    expect(func).toHaveBeenCalledTimes(1);
+    throttledFunc();
 
-    vi.advanceTimersByTime(wait / 2);
-    expect(func).toHaveBeenCalledTimes(1);
+    vi.advanceTimersByTime(DELAY_TIME);
 
-    throttledFunc(); // 대기 시간 이후에 호출되기 때문에 실행
     expect(func).toHaveBeenCalledTimes(2);
-
-    vi.advanceTimersByTime(wait / 2);
-    expect(func).toHaveBeenCalledTimes(2);
-
-    throttledFunc(); // 대기 시간 내에 호출되기 때문에 무시
-    expect(func).toHaveBeenCalledTimes(2);
-
-    vi.advanceTimersByTime(wait / 2);
-    expect(func).toHaveBeenCalledTimes(2);
-
-    throttledFunc(); // 대기 시간 이후에 호출되기 때문에 실행
-    expect(func).toHaveBeenCalledTimes(3);
   });
 
   it('함수가 올바른 인자와 함께 호출되어야 한다', () => {
     const func = vi.fn();
-    const wait = 50;
-    const throttledFunc = throttle(func, wait);
+    const throttledFunc = throttle(func, DELAY_TIME);
 
     throttledFunc('test', 123);
 
@@ -73,49 +41,71 @@ describe('throttle', () => {
 
   it('한 번만 호출될 때는 후행 호출이 트리거되지 않아야 한다', async () => {
     const func = vi.fn();
-    const wait = 50;
 
-    const throttled = throttle(func, wait);
+    const throttled = throttle(func, DELAY_TIME);
 
     throttled();
     expect(func).toBeCalledTimes(1);
 
-    vi.advanceTimersByTime(wait);
+    vi.advanceTimersByTime(DELAY_TIME);
+    expect(func).toBeCalledTimes(1);
+  });
+
+  it('trailing 옵션이 false인 경우 마지막 호출을 무시해야 한다', () => {
+    const func = vi.fn();
+    const throttledFunc = throttle(func, DELAY_TIME, { trailing: false });
+
+    throttledFunc();
+    expect(func).toBeCalledTimes(1);
+
+    throttledFunc();
+
+    vi.advanceTimersByTime(DELAY_TIME);
+    expect(func).toBeCalledTimes(1);
+  });
+
+  it('leading 옵션이 false인 경우 첫 번째 호출을 무시해야 한다', () => {
+    const func = vi.fn();
+    const throttledFunc = throttle(func, DELAY_TIME, { leading: false });
+
+    throttledFunc();
+    expect(func).toBeCalledTimes(0);
+
+    vi.advanceTimersByTime(DELAY_TIME);
+
+    throttledFunc();
     expect(func).toBeCalledTimes(1);
   });
 
   it('AbortController을 통해 초기 호출을 중단할 수 있어야 한다', async () => {
-    const wait = 50;
     const func = vi.fn();
 
     const controller = new AbortController();
     controller.abort();
 
-    const throttled = throttle(func, wait, { signal: controller.signal });
+    const throttled = throttle(func, DELAY_TIME, { signal: controller.signal });
 
     throttled();
     expect(func).toBeCalledTimes(0);
 
-    vi.advanceTimersByTime(wait);
+    vi.advanceTimersByTime(DELAY_TIME);
     expect(func).toBeCalledTimes(0);
   });
 
   it('AbortController을 통해 후행 호출을 중단할 수 있어야 한다', async () => {
-    const wait = 50;
     const func = vi.fn();
     const controller = new AbortController();
 
-    const throttled = throttle(func, wait, { signal: controller.signal });
+    const throttled = throttle(func, DELAY_TIME, { signal: controller.signal });
 
     throttled();
-
     expect(func).toBeCalledTimes(1);
 
     throttled();
 
     controller.abort();
 
-    vi.advanceTimersByTime(wait);
+    vi.advanceTimersByTime(DELAY_TIME);
     expect(func).toBeCalledTimes(1);
   });
 });
