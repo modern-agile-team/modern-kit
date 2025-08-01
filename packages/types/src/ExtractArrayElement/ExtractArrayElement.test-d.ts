10b206d3214400a84192e9cda57e7b8f80f762ab
ssi02014 <ssi02014@naver.com>
2025-08-01 18:11:44 +0900

    fix: type test 코드 수정

--- packages/types/src/ExtractArrayElement/ExtractArrayElement.test-d.ts
+++ packages/types/src/ExtractArrayElement/ExtractArrayElement.test-d.ts
@@ -3,24 +3,20 @@ import { ExtractArrayElement } from '.';
 
 describe('ExtractArrayElement', () => {
   it('배열의 요소 타입을 추출해야 합니다.', () => {
-    const arr = [1, 2, 3];
-
-    expectTypeOf<ExtractArrayElement<typeof arr>>().toEqualTypeOf<number>();
+    expectTypeOf<ExtractArrayElement<number[]>>().toEqualTypeOf<number>();
   });
 
   it('중첩 배열에서 올바르게 타입을 추출해야 합니다.', () => {
-    const arr = [1, 2, [3, 4, [5, 6]]];
-    // (number | (number | number[])[])[]
-
-    expectTypeOf<ExtractArrayElement<typeof arr>>().toEqualTypeOf<number>();
+    expectTypeOf<
+      ExtractArrayElement<(number | (number | number[])[])[]>
+    >().toEqualTypeOf<number>();
   });
 
   it('요소 타입이 혼합된 중첩 배열에서 올바르게 타입을 추출해야 합니다.', () => {
-    const arr = [1, 2, ['string', 4, [true, 6]]];
-    // (number | (string | number | (number | boolean)[])[])[]
-
-    expectTypeOf<ExtractArrayElement<typeof arr>>().toEqualTypeOf<
-      string | number | boolean
-    >();
+    expectTypeOf<
+      ExtractArrayElement<
+        (number | (string | number | (number | boolean)[])[])[]
+      >
+    >().toEqualTypeOf<string | number | boolean>();
   });
 });
