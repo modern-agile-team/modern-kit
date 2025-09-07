7491591b3a08847e5c16001dbdafec43a2475334
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2025-09-07 19:10:15 +0900

    fix(react): useControllableState 타입 수정 (#951)

--- packages/react/src/hooks/useControllableState/index.ts
+++ packages/react/src/hooks/useControllableState/index.ts
@@ -1,72 +1,9 @@
 import { SetStateAction, useCallback, useState } from 'react';
 
 interface UseControllableStateOptions<T> {
-  value?: T;
-  defaultValue?: T;
-}
-
-interface ControlledStateOptions<T> {
-  value: T;
-  defaultValue?: T;
-}
-
-interface UncontrolledStateOptions<T> {
-  value?: T;
+  value?: T | undefined;
   defaultValue: T;
 }
-
-type UseControllableStateReturn<T> = [
-  T | undefined,
-  (nextValue: SetStateAction<T | undefined>) => void
-];
-
-/**
- * @description 제어된 상태와 비제어된 상태를 관리하는 훅입니다.
- *
- * value가 제공되면 제어 상태(controlled mode)로 처리되며, 제공되지 않으면 비제어 상태(uncontrolled mode)로 처리됩니다.
- *
- * controlled mode에서는 value는 외부에서 제어되며,
- * 해당 훅이 반환하는 setValue 함수를 통해 value를 변경할 수 없습니다.
- *
- * uncontrolled mode에서는 defaultValue가 초기값으로 사용되며,
- * 해당 훅이 반환하는 setValue 함수를 통해 value를 변경할 수 있습니다.
- *
- * @template T - 상태의 타입입니다.
- * @param {T} params.value - 제어된 상태의 값입니다.
- * @param {T} params.defaultValue - 비제어된 상태의 값입니다.
- * @returns {UseControllableStateReturn<T>} value와 setValue 함수를 반환합니다.
- *
- * @example
- * const [outerValue, setOuterValue] = useState(1);
- * const [value, setValue] = useControllableState({ value: outerValue });
- *
- * value; // 1
- *
- * setValue(2); // controlled mode에서는 value가 외부에서 제어되기 때문에 변경되지 않음
- * value; // 1
- *
- * setOuterValue(3); // 외부 value 변경
- * value; // 3
- *
- * @example
- * const [value, setValue] = useControllableState({ defaultValue: 1 });
- *
- * // or
- * // const [value, setValue] = useControllableState({
- * //   value: undefined,
- * //   defaultValue: 1
- * // });
- *
- * value; // 1
- *
- * setValue(2); // uncontrolled mode에서는 setValue로 value 변경
- * value; // 2
- */
-export function useControllableState<T>({
-  value,
-  defaultValue,
-}: ControlledStateOptions<T>): UseControllableStateReturn<T>;
-
 /**
  * @description 제어된 상태와 비제어된 상태를 관리하는 훅입니다.
  *
@@ -85,7 +22,7 @@ export function useControllableState<T>({
  *
  * @example
  * const [outerValue, setOuterValue] = useState(1);
- * const [value, setValue] = useControllableState({ value: outerValue });
+ * const [value, setValue] = useControllableState({ value: outerValue, defaultValue: 0 });
  *
  * value; // 1
  *
@@ -96,36 +33,32 @@ export function useControllableState<T>({
  * value; // 3
  *
  * @example
- * const [value, setValue] = useControllableState({ defaultValue: 1 });
+ * const [value, setValue] = useControllableState({ defaultValue: 0 });
  *
  * // or
  * // const [value, setValue] = useControllableState({
  * //   value: undefined,
- * //   defaultValue: 1
+ * //   defaultValue: 0
  * // });
  *
- * value; // 1
+ * value; // 0
  *
- * setValue(2); // uncontrolled mode에서는 setValue로 value 변경
- * value; // 2
+ * setValue(1); // uncontrolled mode에서는 setValue로 value 변경
+ * value; // 1
  */
 export function useControllableState<T>({
   value,
   defaultValue,
-}: UncontrolledStateOptions<T>): UseControllableStateReturn<T>;
-
-export function useControllableState<T>({
-  value,
-  defaultValue,
-}: UseControllableStateOptions<T>): UseControllableStateReturn<T> {
-  const [uncontrolledValue, setUncontrolledValue] = useState<T | undefined>(
-    defaultValue
-  );
+}: UseControllableStateOptions<T>): [
+  T,
+  (nextValue: SetStateAction<T>) => void
+] {
+  const [uncontrolledValue, setUncontrolledValue] = useState<T>(defaultValue);
   const isControlled = value !== undefined;
   const currentValue = isControlled ? value : uncontrolledValue;
 
   const setValue = useCallback(
-    (nextValue: SetStateAction<T | undefined>) => {
+    (nextValue: SetStateAction<T>) => {
       if (!isControlled) {
         setUncontrolledValue(nextValue);
       }
