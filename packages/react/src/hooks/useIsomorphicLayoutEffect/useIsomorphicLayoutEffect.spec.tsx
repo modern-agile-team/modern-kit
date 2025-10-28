c06a30c9d840c57f252867e91613f5a59f008aeb
Gromit (전민재) <64779472+ssi02014@users.noreply.github.com>
2025-10-28 16:15:45 +0900

    chore: vitest v4 (#995)

--- packages/react/src/hooks/useIsomorphicLayoutEffect/useIsomorphicLayoutEffect.spec.tsx
+++ packages/react/src/hooks/useIsomorphicLayoutEffect/useIsomorphicLayoutEffect.spec.tsx
@@ -2,14 +2,14 @@ import { describe, it, expect, afterEach, vi } from 'vitest';
 import { useEffect, useLayoutEffect } from 'react';
 
 describe('useIsomorphicLayoutEffect', () => {
-  const originWindow = global.window;
+  const originWindow = globalThis.window;
 
   afterEach(() => {
     vi.resetModules();
   });
 
   it('서버 환경에서는 useEffect를 사용해야 합니다', async () => {
-    Object.defineProperty(global, 'window', {
+    Object.defineProperty(globalThis, 'window', {
       value: undefined,
     });
 
@@ -18,7 +18,7 @@ describe('useIsomorphicLayoutEffect', () => {
   });
 
   it('클라이언트 환경에서는 useLayoutEffect를 사용해야 합니다', async () => {
-    Object.defineProperty(global, 'window', {
+    Object.defineProperty(globalThis, 'window', {
       value: originWindow,
     });
 
