83baed068773beab35b7900a88d93bf97a2361f7
ssi02014 <ssi02014@users.noreply.github.com>
2025-12-03 06:22:59 +0000

    Deploying to gh-pages from @ modern-agile-team/modern-kit@c4e2734f89c59978bb2af698dbf2199818fc2294 🚀

--- assets/js/5e95c892.0480528e.js
+++
@@ -1,1 +0,0 @@
-"use strict";(self.webpackChunk_modern_kit_docs=self.webpackChunk_modern_kit_docs||[]).push([[2028],{2403:(e,s,r)=>{r.r(s),r.d(s,{default:()=>o});r(4382);var c=r(7164),d=r(7210),n=r(9605),t=r(560),u=r(8111),a=r(6206);function o(e){return(0,a.jsx)(d.e3,{className:(0,c.A)(n.G.wrapper.docsPages),children:(0,a.jsx)(u.A,{children:(0,t.v)(e.route.routes)})})}}}]);
